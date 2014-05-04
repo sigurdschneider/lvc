96fd00c01dccd768e50ba2fabe256d1f8ffb5254
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-04 16:31:10 +0200

    Consider cpu time instead of wall-clock (real) time.

--- time.sh
+++ time.sh
@@ -9,7 +9,7 @@ print "#{green('>>>')} #{cmd}\n"
 
 start = Time.now
 #system(cmd)
-cstdin, cstdout, cstderr, waitthr = Open3.popen3(cmd)
+cstdin, cstdout, cstderr, waitthr = Open3.popen3("bash -c \"time #{cmd}\"")
 path = #{ARGV.last}.split('/')
 #Process.wait(waitthr.pid)
 waitthr.join
@@ -18,12 +18,18 @@ File.open("#{ARGV.last}.time", 'w') do |file|
   file.puts "#{time}"
 end
 
-
 success = waitthr.value.to_i == 0
 color = lambda { |s| success ? (green s) : (red s) }
 
-print color.call("<<<"), " #{cmd} \t\t ", color.call("#{time.round(2)}"), "\n"
-IO.copy_stream(cstdout, STDOUT)
-IO.copy_stream(cstderr, STDERR)
+sout = cstdout.read
+serr = cstderr.read
+print sout
+user = serr.match(/.*user[ \t]*([0123456789]+)m([0123456789\.]+)s.*/m)
+sys = serr.match(/.*sys[ \t]*([0123456789]+)m([0123456789\.]+)s.*/m)
+cpu = user[1].to_f * 60 + user[2].to_f + sys[1].to_f * 60 + sys[2].to_f
+#IO.copy_stream(cstdout, STDOUT)
+#IO.copy_stream(cstderr, STDERR)
+
+print color.call("<<<"), " #{cmd} \t\t ", color.call("#{cpu.round(2)} / #{time.round(2)}"), "\n"
 
 exit success 
