5a6f4abf707d61b26c995fc34ae44b0b22b20bd4
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-04 14:12:24 +0200

    Add color and time measure to Makefile.

--- configure.sh
+++ configure.sh
@@ -3,3 +3,8 @@
 SOURCES=`find -type f -iname '*.v' -printf '%P\n'`
 coq_makefile -R . Lvc extraction $SOURCES > Makefile
 sed -i '/.\/extraction:/c\.\/extraction: Compiler.vo' Makefile
+sed -i 's/$(COQC) $(COQDEBUG) $(COQFLAGS)/@$(COQC) $(COQDEBUG) $(COQFLAGS)/' Makefile
+sed -i '/COQCHK?=$(COQBIN)coqchk/a COQC:=./time.sh $(COQC)' Makefile
+
+
+
--- time.sh
+++ time.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/ruby
+require 'term/ansicolor'
+require 'open3'
+
+include Term::ANSIColor
+
+cmd = ARGV.join(' ')
+print "#{green('>>>')} #{cmd}\n"
 
 start = Time.now
-system(ARGV.join(' '))
+#system(cmd)
+cstdin, cstdout, cstderr, waitthr = Open3.popen3(cmd)
 path = #{ARGV.last}.split('/')
+#Process.wait(waitthr.pid)
+waitthr.join
+time = Time.now - start
 File.open("#{ARGV.last}.time", 'w') do |file|
-  file.puts "#{Time.now - start}"
+  file.puts "#{time}"
 end
+
+
+success = waitthr.value.to_i == 0
+color = lambda { |s| success ? (green s) : (red s) }
+
+print color.call("<<<"), " #{cmd} \t\t ", color.call("#{time.round(2)}"), "\n"
+IO.copy_stream(cstdout, STDOUT)
+IO.copy_stream(cstderr, STDERR)
+
+exit success 
