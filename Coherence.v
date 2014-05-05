5e89d834cb68bc5c3eb9f49df61f72032bb74b15
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-05 15:30:45 +0200

    Fix Coherence for expressions everywhere.

--- Coherence.v
+++ Coherence.v
@@ -1,7 +1,7 @@
 Require Import CSet Le.
 
 Require Import Plus Util AllInRel Map.
-Require Import Val Var Env EnvTy IL Annotation Liveness ParamsMatch Restrict Sim.
+Require Import Val Var Env EnvTy IL Annotation Liveness ParamsMatch Restrict Sim MoreExp.
 
 Set Implicit Arguments.
 
@@ -9,53 +9,53 @@ Set Implicit Arguments.
 Inductive ssa : set var -> stmt -> set var -> Prop :=
   | ssaExp x e s D D'
     : x ∉ D 
-      -> (Exp.freeVars e) ⊆ D
+      -> Exp.freeVars e ⊆ D
       -> ssa (D ∪ {{x}}) s D'
       -> ssa D (stmtExp x e s) D'
-  | ssaIf  D D' Ds Dt x s t
-    : x ∈ D
-    -> Ds ∩ Dt [=] D
-    -> Ds ∪ Dt [=] D'
-    -> ssa D s Ds 
-    -> ssa D t Dt
-    -> ssa D (stmtIf x s t) D'
-  | ssaRet D D' x
-    : x ∈ D
-    -> D [=] D'
-    -> ssa D (stmtReturn x) D'
+  | ssaIf  D D' Ds Dt e s t
+    : Exp.freeVars e ⊆ D
+      -> Ds ∩ Dt [=] D
+      -> Ds ∪ Dt [=] D'
+      -> ssa D s Ds 
+      -> ssa D t Dt
+      -> ssa D (stmtIf e s t) D'
+  | ssaRet D D' e
+    : Exp.freeVars e ⊆ D
+      -> D [=] D'
+      -> ssa D (stmtReturn e) D'
   | ssaGoto D D' f Y
-    : of_list Y ⊆ D 
-    -> D [=] D'
-    -> ssa D (stmtGoto f Y) D'
+    : list_union (List.map Exp.freeVars Y) ⊆ D 
+      -> D [=] D'
+      -> ssa D (stmtGoto f Y) D'
   | ssaLet D D' s t Ds Dt Z
     : of_list Z ∩ D [=] ∅
-    -> Ds ∩ Dt [=] D
-    -> Ds ∪ Dt [=] D'
-    -> ssa (of_list Z ∪ D) s Ds 
-    -> ssa D t Dt
-    -> ssa D (stmtLet Z s t) D'.
+      -> Ds ∩ Dt [=] D
+      -> Ds ∪ Dt [=] D'
+      -> ssa (of_list Z ∪ D) s Ds 
+      -> ssa D t Dt
+      -> ssa D (stmtLet Z s t) D'.
 
 Lemma ssa_ext D1 D1' s D2 D2' 
   : D1 [=] D1' -> D2 [=] D2'
   -> ssa D1 s D2
   -> ssa D1' s D2'.
 Proof. 
-  intros. general induction H1.
-  econstructor. rewrite <- H2; eauto. rewrite <- H2; eauto.
-  eapply IHssa; eauto. rewrite H2; reflexivity. 
+  intros. general induction H1; simpl; eauto using ssa.
+  - econstructor. rewrite <- H2; eauto. rewrite <- H2; eauto.
+    eapply IHssa; eauto. rewrite H2; reflexivity. 
   
-  econstructor. rewrite <- H2; eauto. rewrite <- H2; eauto. rewrite <- H3; eauto. 
-  eapply IHssa1; eauto; reflexivity.
-  eapply IHssa2; eauto; reflexivity.
+  - econstructor. rewrite <- H2; eauto. rewrite <- H2; eauto. rewrite <- H3; eauto. 
+    eapply IHssa1; eauto; reflexivity.
+    eapply IHssa2; eauto; reflexivity.
   
-  econstructor. rewrite <- H1; eauto. rewrite <- H1, H0; eauto.
-  econstructor. rewrite <- H1; eauto. rewrite <- H1, H0; eauto.
+  - econstructor. rewrite <- H1; eauto. rewrite <- H1, H0; eauto.
+  - econstructor. rewrite <- H1; eauto. rewrite <- H1, H0; eauto.
 
-  econstructor. rewrite <- H2; eauto.
-  rewrite <- H2; eauto.
-  rewrite <- H3; eauto.
-  eapply IHssa1. rewrite <- H2; reflexivity. reflexivity.
-  eapply IHssa2; eauto. reflexivity.  
+  - econstructor. rewrite <- H2; eauto.
+    rewrite <- H2; eauto.
+    rewrite <- H3; eauto.
+    eapply IHssa1. rewrite <- H2; reflexivity. reflexivity.
+    eapply IHssa2; eauto. reflexivity.  
 Qed.
 
 Add Parametric Morphism : ssa
@@ -82,61 +82,86 @@ Lemma ssa_freeVars D D' s
   : ssa D s D' -> freeVars s ⊆ D.
 Proof.
   intros. general induction H; simpl; eauto.
-  cset_tac; intuition. specialize (IHssa _ H2). cset_tac; intuition.
-  cset_tac; intuition. rewrite H5; eauto.
-  cset_tac; eauto.
-  cset_tac; intuition. specialize (IHssa1 _ H4). cset_tac; intuition.
+  - rewrite H0, IHssa. cset_tac; intuition.
+  - rewrite H, IHssa1, IHssa2. cset_tac; intuition.
+  - rewrite IHssa1, IHssa2. cset_tac; intuition.
+Qed.
+
+Lemma list_union_minus_dist Y D'' s s'
+  :
+    s \ D'' [=] s'
+ ->  fold_left union (List.map Exp.freeVars Y) s \ D''
+[=] fold_left union (List.map (fun y : exp => Exp.freeVars y \ D'') Y) s'.
+Proof.
+  general induction Y; simpl; eauto.
+  - eapply IHY. rewrite <- H. clear_all.
+    cset_tac; intuition.
+Qed.
+
+Instance fold_left_union_morphism X `{OrderedType X}:
+  Proper (list_eq Equal ==> Equal ==> Equal) (fold_left union).
+Proof.
+  unfold Proper, respectful; intros.
+  general induction H0; simpl; eauto.
+  - rewrite IHlist_eq; eauto. reflexivity.
+    rewrite H0, H2. reflexivity.
 Qed.
 
 Lemma ssa_minus D D' D'' s
   : notOccur D'' s -> ssa D s D' -> ssa (D \ D'') s (D' \ D'').
 Proof.
   intros. general induction H0. 
-  inv H2. econstructor. intro. cset_tac; eauto.
-  eapply Exp.notOccur_freeVars in H8; eauto. rewrite meet_comm in H8.
-  rewrite <- H0. rewrite minus_inane_set; eauto. reflexivity.
-  assert ((D \ D'') ∪ {{x}} [=] (D ∪ {{x}}) \ D''). cset_tac. 
-  unfold Equivalence.equiv, _eq; simpl. 
-  split; intuition. subst; eauto. rewrite H3. eapply IHssa; eauto.
-  
-  inv H2. rewrite <- H0, <- H1, minus_dist_union, minus_dist_intersection.
-  econstructor. rewrite <- H0 in H. cset_tac; eauto. reflexivity. reflexivity.
-  rewrite <- minus_dist_intersection, H0. eapply IHssa1; eauto.
-  rewrite <- minus_dist_intersection, H0. eapply IHssa2; eauto.
-  econstructor. inv H1. eauto using in_in_minus. rewrite H0. reflexivity.
-  econstructor. inv H1. hnf; intros. eapply in_in_minus; eauto. 
-
-  intros. cset_tac; firstorder.
-  cset_tac; firstorder.
-  
-  inv H2. rewrite <- H0, <- H1. rewrite minus_dist_union. rewrite minus_dist_intersection. 
-  econstructor. rewrite <- minus_dist_intersection. rewrite H0.
-  eapply incl_eq. eapply incl_empty. rewrite <- H. 
-  eapply incl_meet_lr; eauto. reflexivity. eapply minus_incl.
-  reflexivity. reflexivity.  
-  rewrite <- minus_dist_intersection. 
-  rewrite <- (minus_inane_set _ _ _ H6). rewrite <- minus_dist_union.
-  rewrite H0. eapply IHssa1; eauto.
-  rewrite <- minus_dist_intersection. rewrite H0. eapply IHssa2; eauto.
+  - inv H2. econstructor. intro. cset_tac; eauto.
+    eapply Exp.notOccur_freeVars in H8; eauto. rewrite meet_comm in H8.
+    rewrite <- H0. rewrite minus_inane_set; eauto. reflexivity.
+    assert ((D \ D'') ∪ {{x}} [=] (D ∪ {{x}}) \ D''). cset_tac. 
+    unfold Equivalence.equiv, _eq; simpl. 
+    split; intuition. subst; eauto. rewrite H3. eapply IHssa; eauto.
+  - inv H2. eapply Exp.notOccur_freeVars in H6.
+    econstructor; eauto. rewrite <- H. cset_tac; intuition; eauto.
+    rewrite <- minus_dist_intersection. rewrite H0. reflexivity.
+    rewrite <- minus_dist_union. rewrite H1. reflexivity.
+  - inv H1. eapply Exp.notOccur_freeVars in H3. econstructor.
+    cset_tac; intuition; eauto. rewrite H0; reflexivity.
+  - inv H1. 
+    econstructor. rewrite <- H. unfold list_union.
+    rewrite list_union_minus_dist; eauto;try reflexivity.
+    eapply incl_set_left.
+    eapply fold_left_union_morphism.
+    eapply map_ext_get.
+    intros; simpl. exploit H3; eauto. eapply Exp.notOccur_freeVars in X.
+    revert X; clear_all. cset_tac; intuition; eauto.
+    cset_tac; intuition.
+    rewrite H0. reflexivity.
+  - inv H2. 
+    rewrite <- H0, <- H1. rewrite minus_dist_union. rewrite minus_dist_intersection. 
+    econstructor. rewrite <- minus_dist_intersection. rewrite H0.
+    eapply incl_eq. eapply incl_empty. rewrite <- H. 
+    eapply incl_meet_lr; eauto. reflexivity. eapply minus_incl.
+    reflexivity. reflexivity.  
+    rewrite <- minus_dist_intersection. 
+    rewrite <- (minus_inane_set _ _ _ H6). rewrite <- minus_dist_union.
+    rewrite H0. eapply IHssa1; eauto.
+    rewrite <- minus_dist_intersection. rewrite H0. eapply IHssa2; eauto.
 Qed.
 
 (* shadowing free *)
 Inductive shadowing_free : set var -> stmt -> Prop :=
   | shadowing_freeExp x e s D
     : x ∉ D 
-      -> (Exp.freeVars e) ⊆ D
+      -> Exp.freeVars e ⊆ D
       -> shadowing_free (D ∪ {{x}}) s
       -> shadowing_free D (stmtExp x e s)
-  | shadowing_freeIf D x s t
-    : x ∈ D
+  | shadowing_freeIf D e s t
+    : Exp.freeVars e ⊆ D
     -> shadowing_free D s 
     -> shadowing_free D t
-    -> shadowing_free D (stmtIf x s t)
-  | shadowing_freeRet D x
-    : x ∈ D
-    -> shadowing_free D (stmtReturn x)
+    -> shadowing_free D (stmtIf e s t)
+  | shadowing_freeRet D e
+    : Exp.freeVars e ⊆ D
+    -> shadowing_free D (stmtReturn e)
   | shadowing_freeGoto D f Y
-    : of_list Y ⊆ D 
+    : list_union (List.map Exp.freeVars Y) ⊆ D 
     -> shadowing_free D (stmtGoto f Y)
   | shadowing_freeLet D s t Z
     : of_list Z ∩ D [=] ∅
@@ -148,12 +173,12 @@ Inductive srd : list (option (set var)) -> stmt -> ann (set var) -> Prop :=
  | srdExp DL x e s lv al
     : srd (restrict DL (lv\{{x}})) s al
     -> srd DL (stmtExp x e s) (ann1 lv al)
-  | srdIf DL x s t lv als alt
-    :  srd DL s als
+  | srdIf DL e s t lv als alt
+    : srd DL s als
     -> srd DL t alt
-    -> srd DL (stmtIf x s t) (ann2 lv als alt)
-  | srdRet x DL lv
-    : srd DL (stmtReturn x) (ann0 lv)
+    -> srd DL (stmtIf e s t) (ann2 lv als alt)
+  | srdRet e DL lv
+    : srd DL (stmtReturn e) (ann0 lv)
   | srdGoto DL lv G' f Y
     : get DL (counted f) (Some G')
     -> G' ⊆ lv
@@ -432,24 +457,28 @@ Proof.
   eapply RA; eauto.
 Qed.
 
-Lemma rd_agree_update_list DL L E E' (G G':set var) Z Y n
+Lemma rd_agree_update_list DL L E E' (G G':set var) Z n vl
  (RA:rd_agree DL L E)
  (ZD:of_list Z ∩ G' [=] ∅)
- (LEQ:length Z = length Y)
+ (LEQ:length Z = length vl)
  (AG:agree_on G' E E')
-: rd_agree (restrict (drop n DL) G') (drop n L) (updE E' E Z Y).
+: rd_agree (restrict (drop n DL) G') (drop n L) (E'[Z <-- vl]).
 Proof.
   hnf; intros.
-  assert (G'0 ⊆ G'). eapply bounded_get; eauto. eapply bounded_restrict; reflexivity.
+  assert (G'0 ⊆ G'). { 
+    eapply bounded_get; eauto. eapply bounded_restrict; reflexivity.
+  } 
   assert (G'0 [=] G'0 \ of_list Z) by (split; cset_tac; intuition eauto).
-  rewrite H2. unfold updE. eapply update_with_list_agree_minus. rewrite lookup_list_length; eauto.
+  rewrite H2. eapply update_with_list_agree_minus; eauto. 
 
   unfold restrict in H0. rewrite drop_map in H0. 
   eapply get_drop in H. eapply get_drop in H0.
   eapply map_get_4 in H0; dcr.
+  hnf in RA.
   eapply agree_on_trans. eapply agree_on_sym; eauto using agree_on_incl.
   eapply RA; eauto. eapply restr_iff in H4; dcr; subst; eauto.
 Qed.
+
 
 Inductive approx' 
   : list (option (set var)) -> F.block -> Prop :=
@@ -499,7 +528,9 @@ Proof.
   + provide_invariants_2. specialize (H3 _ H4); dcr.
     rewrite H2 in H7. simpl in *.
     do 3 eexists; repeat split; simpl; eauto.
-    eapply rd_agree_update_list; eauto. eapply (RA _ _ _ H1 H).
+    pose proof (RA _ _ _ H1 H). simpl in *.
+    eapply rd_agree_update_list; eauto. 
+    exploit omap_length; eauto. congruence.
 
   + inv LV. do 3 eexists; repeat split; eauto.
     hnf; intros.
@@ -550,40 +581,42 @@ Proof.
     eapply agree_on_update_same; eapply agree_on_incl; eauto.
     eapply simE; try eapply star_refl; eauto; stuck.
     erewrite <- exp_eval_live in def; eauto. congruence.
-  + case_eq (val2bool (E x)); intros.
-    eapply simS; try eapply plusO. 
-    econstructor; eauto. 
-    econstructor. rewrite <- AG; eauto.
+  + case_eq (exp_eval E e); intros.
+    exploit exp_eval_live_agree; eauto.
+    case_eq (val2bool v); intros.
+    one_step.
     eapply srdSim_sim; econstructor; eauto using agree_on_incl.
     one_step.
     eapply srdSim_sim; econstructor; eauto using agree_on_incl.
-  + eapply simE; try eapply star_refl; simpl; eauto; try stuck.
+    exploit exp_eval_live_agree; eauto.
+    no_step.
+  + no_step. simpl. eapply exp_eval_live; eauto.
   + decide (length Z0 = length Y).
-    eapply simS; try eapply plusO.
-    econstructor; simpl; eauto. simpl; eauto.
-    pose proof (map_get_1 stripB H1).
-    econstructor; eauto. eauto.
-    eapply srdSim_sim. rewrite drop_strip.  
-    simpl. simpl counted in *.
-    specialize (H3 _ H5); dcr. rewrite H2 in A1,H11.
-    econstructor; simpl; eauto using approx_restrict.
-    eapply rd_agree_update_list; eauto.
-    eapply (RA _ _ _ H1 H).
-
-    simpl; unfold updE. 
-    erewrite lookup_list_agree; eauto using agree_on_incl.
-    eapply update_with_list_agree. rewrite H10.
-    rewrite union_comm. rewrite union_minus_remove.
-    pose proof (RA _ _ G' H1 H); dcr. simpl in *.
-    eapply agree_on_sym. eapply agree_on_incl; eauto using incl_minus.
-    eapply agree_on_trans; eauto. eapply agree_on_sym. hnf in RA.
-    eapply agree_on_incl; eauto.
-    rewrite lookup_list_length; eauto.
-    no_step. get_functional; subst; simpl in *; congruence.
-    unfold strip in *.
-    edestruct map_get_4; eauto; dcr; get_functional; subst; simpl in *. 
-    congruence.
-  + eapply simS; try (eapply plusO; econstructor).
+    case_eq (omap (exp_eval E) Y); intros.
+    - exploit omap_exp_eval_live_agree; eauto.
+      pose proof (map_get_1 stripB H1).
+      one_step.
+      eapply srdSim_sim. rewrite drop_strip.  
+      simpl. simpl counted in *.
+      specialize (H3 _ H5); dcr. rewrite H2 in A1,H13.
+      econstructor; simpl; eauto using approx_restrict.
+      eapply rd_agree_update_list; eauto.
+      exploit omap_length; eauto. congruence.
+      eapply (RA _ _ _ H1 H).
+      eapply update_with_list_agree. rewrite H12.
+      rewrite union_comm. rewrite union_minus_remove.
+      pose proof (RA _ _ G' H1 H); dcr. simpl in *.
+      eapply agree_on_sym. eapply agree_on_incl; eauto using incl_minus.
+      eapply agree_on_trans; eauto. eapply agree_on_sym. hnf in RA.
+      eapply agree_on_incl; eauto.
+      exploit omap_length; eauto. congruence.
+    - exploit omap_exp_eval_live_agree; eauto.
+      no_step. 
+    - no_step. get_functional; subst; simpl in *; congruence.
+      unfold strip in *.
+      edestruct map_get_4; eauto; dcr; get_functional; subst; simpl in *. 
+      congruence.
+  + one_step.
     eapply srdSim_sim; econstructor; eauto.
     hnf; intros.
     destruct n; inv H1; inv H2. simpl.
