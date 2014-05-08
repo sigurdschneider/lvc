cff965b6aa2661f334a68ba6941de32e0ec04d57
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-08 13:24:53 +0200

    Add rudimentary patterns.

---
+++ IL/Patterns.v
@@ -0,0 +1,67 @@
+Require Import List.
+Require Export Util Relations Get Drop Var Val Exp Env Map CSet AutoIndTac MoreList.
+
+Set Implicit Arguments.
+
+(* non-recursive patterns *)
+Inductive pattern :=
+  | PatternScore
+  | PatternVar (x:var)
+  | PatternList (Z:list var).
+
+Fixpoint patternVars (p:pattern) : set var :=
+  match p with
+    | PatternScore => ∅
+    | PatternVar x => {{x}}
+    | PatternList Z => of_list Z
+  end.
+
+Definition patternMatchList 
+         (patternMatch: pattern -> val -> option (list var * list val)) :=
+  fix patternMatchList (Z:list pattern) (vl:list val) {struct Z} : option (list var * list val) := 
+  match Z, vl with 
+    | p::Z', v::vl => 
+      mdo l <- patternMatch p v; 
+        let (varl, vall) := l in
+        mdo l' <- patternMatchList Z' vl;
+          let (varl', vall') := l' in
+          Some ((varl++varl')%list, (vall++vall')%list)
+    | nil, nil => Some (nil, nil)
+  | _, _ => None
+  end.
+
+Fixpoint patternMatch (p:pattern) (v:val) {struct p} : option (list var * list val) :=
+  match p, v with 
+    | PatternScore, _ => Some (nil, nil)
+    | PatternVar x, v => Some (x::nil, v::nil)
+(*  | PatternList Z, ValTpl vl => if [ length Z = length vl] then 
+                                   Some (Z, vl)
+                                 else
+                                   None *)
+    | _, _ => None
+  end.
+
+Definition patterneq (p q:pattern) 
+  := forall v, patternMatch p v <> None <-> patternMatch q v <> None.
+
+Instance patterneq_Equivalence_instance : Equivalence patterneq.
+constructor; firstorder.
+Defined.
+
+Lemma patterneq_some p q v ZVL
+: patterneq p q -> patternMatch p v = Some ZVL -> exists ZVL, patternMatch q v = Some ZVL.
+Proof.
+  intros. specialize (H v).
+  destruct (patternMatch q v).
+  - eexists; eauto.
+  - exfalso; firstorder. eapply H; congruence.
+Qed.
+
+
+
+(* 
+*** Local Variables: ***
+*** coq-load-path: (("../" "Lvc")) ***
+*** End: ***
+*)
+
