b2c2d7cfac3af1d0b42ad9a67c27d02db0d4771b
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-17 14:54:56 +0200

    Update Alpha for external events (syscalls).

--- Alpha.v
+++ Alpha.v
@@ -1,4 +1,4 @@
-Require Import Util Map Env EnvTy Exp IL AllInRel Sim Computable.
+Require Import Util Map Env EnvTy Exp IL AllInRel Bisim Computable.
 
 Import F.
 
@@ -20,19 +20,27 @@ Inductive alpha : env var -> env var -> stmt -> stmt -> Prop :=
   -> alpha ra ira s s'
   -> alpha ra ira t t'
   -> alpha ra ira (stmtIf e s t) (stmtIf e' s' t')
+| alpha_extern ra ira x y f Y Y' s s'
+  : length Y = length Y'
+  -> (forall n x y, get Y n x -> get Y' n y -> alpha_exp ra ira x y)
+  -> alpha (ra[x<-y]) (ira[y <- x]) s s'
+  -> alpha ra ira (stmtExtern x f Y s) (stmtExtern y f Y' s')
 | alpha_let ra ira s s' Z Z' t t'
   : length Z = length Z'
   -> alpha (ra [ Z <-- Z']) (ira [ Z' <-- Z ]) s s'
   -> alpha ra ira t t' -> alpha ra ira (stmtLet Z s t) (stmtLet Z' s' t').
 
 Global Instance alpha_morph
- : Proper (feq ==> feq ==> eq ==> eq ==> impl) alpha.
+ : Proper ((@feq _ _ _eq) ==> (@feq _ _ _eq) ==> eq ==> eq ==> impl) alpha.
 Proof.
   unfold respectful, Proper, impl; intros; subst.
   general induction H3; econstructor; eauto using alpha_exp_morph.
   - eapply IHalpha.
     + rewrite H0; reflexivity.
     + rewrite H1; reflexivity.
+  - eapply IHalpha.
+    + rewrite H1; reflexivity.
+    + rewrite H2; reflexivity.
   - eapply IHalpha1.
     + rewrite H0; reflexivity.
     + rewrite H1; reflexivity.
@@ -46,6 +54,9 @@ Proof.
     rewrite update_id; eauto.
   - constructor; eauto using lookup_id.
     + intros. get_functional; subst; eauto using alpha_exp_refl.
+  - econstructor; eauto.
+    + intros. get_functional; subst; eauto using alpha_exp_refl.
+    + rewrite update_id; eauto.
   - econstructor; try rewrite update_with_list_id; eauto using length_eq_refl.
 Qed.
 
@@ -54,6 +65,14 @@ Lemma alpha_sym ϱ ϱ' s s'
   ->alpha ϱ' ϱ s' s.
 Proof.
   intros. general induction H; eauto using alpha, length_eq_sym, alpha_exp_sym.
+Qed.
+
+Lemma inverse_on_dead_update X `{OrderedType X} Y `{OrderedType Y} (ra:X->Y) ira (x:X) (y:Y) s
+: inverse_on s (ra [x <- y]) (ira [y <- x])
+  -> inverse_on (s \ {{x}}) ra ira.
+Proof.
+  intros. hnf; intros. cset_tac; dcr.
+  specialize (H1 _ H3). lud.
 Qed.
 
 Lemma alpha_inverse_on  ϱ ϱ' s t
@@ -66,53 +85,74 @@ Proof.
     edestruct get_length_eq; eauto. subst.
     eapply alpha_exp_inverse_on; eauto.
     inv i.
-  + hnf; intros. cset_tac. destruct H0; dcr.
-    specialize (IHA x0 H1). lud.
-    eapply alpha_exp_inverse_on; eauto.
+  + eapply inverse_on_union;
+    eauto using inverse_on_dead_update, alpha_exp_inverse_on.
   + eapply inverse_on_union; try eapply inverse_on_union; eauto using alpha_exp_inverse_on.
+  + eapply inverse_on_union; eauto using inverse_on_dead_update.
+    hnf; intros.
+    edestruct (list_union_get (List.map Exp.freeVars Y)) as [[? []]|]; eauto; dcr.
+    edestruct map_get_4; eauto; dcr; subst.
+    edestruct get_length_eq; try eapply H; eauto.
+    eapply alpha_exp_inverse_on; eauto.
+    inv i.
   + eapply inverse_on_union; eauto.
     eapply update_with_list_inverse_on; eauto.
 Qed.
 
 Lemma alpha_agree_on_morph f g ϱ ϱ' s t
   : alpha ϱ ϱ' s t
-  -> agree_on (lookup_set ϱ (freeVars s)) g ϱ'
-  -> agree_on (freeVars s) f ϱ
+  -> agree_on _eq (lookup_set ϱ (freeVars s)) g ϱ'
+  -> agree_on _eq (freeVars s) f ϱ
   -> alpha f g s t.
 Proof.
   intros. general induction H; simpl in * |- *; eauto using alpha, alpha_exp_agree_on_morph.
-  + econstructor; intros; eauto.
+  - econstructor; intros; eauto.
     eapply alpha_exp_agree_on_morph; eauto.
     eapply agree_on_incl; eauto.
     eapply lookup_set_incl; try now intuition; eauto using get_list_union_map.
     eapply agree_on_incl; eauto using get_list_union_map.
-  + econstructor.
+  - econstructor.
     eapply alpha_exp_agree_on_morph; eauto.
     eapply agree_on_incl; eauto.
     eapply lookup_set_incl; try now intuition; eauto using get_list_union_map.
     eapply agree_on_incl; eauto. cset_tac; intuition.
     eapply IHalpha; eauto.
-    - eapply agree_on_update_same. eapply agree_on_incl; eauto.
+    + eapply agree_on_update_same. reflexivity. eapply agree_on_incl; eauto.
       hnf; intros. eapply lookup_set_spec. intuition.
       cset_tac; eqs. eapply lookup_set_spec in H4.
       destruct H4; dcr. eexists x0. lud.
       split; eqs. cset_tac. left. split; eauto. intuition.
-    - eapply agree_on_update_same. eapply agree_on_incl; eauto.
+    + eapply agree_on_update_same. reflexivity. eapply agree_on_incl; eauto.
       cset_tac; eqs; intuition.
-  + econstructor; eauto.
-    - eapply alpha_exp_agree_on_morph; eauto.
+  - econstructor; eauto.
+    + eapply alpha_exp_agree_on_morph; eauto.
       eapply agree_on_incl; eauto.
       eapply lookup_set_incl; try now intuition; eauto using get_list_union_map.
       eapply agree_on_incl; eauto using get_list_union_map. cset_tac; intuition.
-    - eapply IHalpha1. eapply agree_on_incl; eauto.
+    + eapply IHalpha1. eapply agree_on_incl; eauto.
       eapply lookup_set_incl; intuition.
       cset_tac; intuition.
       eapply agree_on_incl; eauto. cset_tac; intuition.
-    - eapply IHalpha2. eapply agree_on_incl; eauto.
+    + eapply IHalpha2. eapply agree_on_incl; eauto.
       eapply lookup_set_incl; intuition.
       cset_tac; intuition.
       eapply agree_on_incl; eauto. cset_tac; intuition.
-  + econstructor; eauto.
+  - econstructor; eauto.
+    + intros. eapply alpha_exp_agree_on_morph; eauto.
+      eapply agree_on_incl; eauto.
+      eapply lookup_set_incl; try now intuition.
+      rewrite get_list_union_map; eauto. eapply incl_right.
+      eapply agree_on_incl; eauto using get_list_union_map.
+      rewrite get_list_union_map; eauto. eapply incl_right.
+    + eapply IHalpha.
+      eapply agree_on_update_same. reflexivity. eapply agree_on_incl; eauto.
+      hnf; intros. eapply lookup_set_spec. intuition.
+      cset_tac; eqs. eapply lookup_set_spec in H5.
+      destruct H5; dcr. eexists x0. lud.
+      split; eqs. cset_tac. left. split; eauto. intuition.
+      eapply agree_on_update_same. reflexivity. eapply agree_on_incl; eauto.
+      cset_tac; eqs; intuition.
+  - econstructor; eauto.
     eapply IHalpha1; eauto.
     eapply update_with_list_agree; eauto using length_eq_sym.
     eapply agree_on_incl; eauto.
@@ -134,7 +174,7 @@ Qed.
 Lemma alpha_inverse_on_agree f g ϱ ϱ' s t
   : alpha ϱ ϱ' s t
   -> inverse_on (freeVars s) f g
-  -> agree_on (freeVars s) f ϱ
+  -> agree_on _eq (freeVars s) f ϱ
   -> alpha f g s t.
 Proof.
   intros. eapply alpha_agree_on_morph; eauto.
@@ -150,17 +190,26 @@ Lemma alpha_trans ϱ1 ϱ1' ϱ2 ϱ2' s s' s''
   : alpha ϱ1 ϱ1' s s' -> alpha ϱ2 ϱ2' s' s'' -> alpha (ϱ1 ∘ ϱ2) (ϱ2' ∘ ϱ1') s s''.
 Proof.
   intros. general induction H; invt alpha; eauto using alpha_exp_trans, alpha.
-  + econstructor; eauto. etransitivity; eauto.
+  - econstructor; eauto. etransitivity; eauto.
     intros. edestruct (get_length_eq _ H2 H); eauto.
     eapply alpha_exp_trans; eauto.
-  + econstructor; eauto using alpha_exp_trans.
+  - econstructor; eauto using alpha_exp_trans.
     specialize (IHalpha _ _ _ H9).
     eapply alpha_inverse_on in H0.
     eapply alpha_inverse_on_agree; eauto.
     eapply inverse_on_comp; eauto. eapply alpha_inverse_on; eauto.
     symmetry. eapply lookup_set_agree_on_comp. intuition.
     eapply inverse_on_update_lookup_set. intuition. eauto.
-  + econstructor; eauto. congruence.
+  - econstructor; eauto. congruence.
+    + intros. edestruct (get_length_eq _ H3 H); eauto.
+      eapply alpha_exp_trans; eauto.
+    + specialize (IHalpha _ _ _ H12).
+      eapply alpha_inverse_on in H1.
+      eapply alpha_inverse_on_agree; eauto.
+      eapply inverse_on_comp; eauto. eapply alpha_inverse_on; eauto.
+      symmetry. eapply lookup_set_agree_on_comp. intuition.
+      eapply inverse_on_update_lookup_set. intuition. eauto.
+  - econstructor; eauto. congruence.
     specialize (IHalpha1 _ _ _ H10).
     eapply alpha_inverse_on_agree; eauto.
     eapply alpha_inverse_on in IHalpha1.
@@ -265,35 +314,35 @@ Inductive alphaSim : F.state -> F.state -> Prop :=
 
 
 Lemma alphaSim_sim σ1 σ2
-: alphaSim σ1 σ2 -> sim σ1 σ2.
+: alphaSim σ1 σ2 -> bisim σ1 σ2.
 Proof.
   revert σ1 σ2; cofix; intros.
   destruct H; inversion AE; subst ra0 ira0; simpl in * |- *;
   try subst s s'; simpl in * |- *.
-  + eapply simE; try eapply star_refl; simpl; eauto using alpha_exp_eval; try stuck.
-  + destruct (get_dec L (counted l)) as [[[Eb Zb sb]]|].
+  - no_step. simpl. eapply alpha_exp_eval; eauto.
+  - destruct (get_dec L (counted l)) as [[[Eb Zb sb]]|].
     decide (length Zb = length Y).
     case_eq (omap (exp_eval E) X); intros.
-    - provide_invariants_3. simpl in *.
+    + provide_invariants_3. simpl in *.
       one_step; simpl; try congruence; eauto.
       erewrite omap_agree_2; eauto. intros. symmetry.
       eapply alpha_exp_eval. eapply H0; eauto; eauto. hnf; intros; eauto.
       simpl. eapply alphaSim_sim; econstructor; eauto.
       eapply envCorr_update_list; eauto. exploit omap_length; eauto.
       rewrite map_length. congruence.
-    - no_step. erewrite omap_agree_2 in H1; try eapply H.
+    + no_step. erewrite omap_agree_2 in H1; try eapply H.
       erewrite H1 in def. congruence.
       intros. eapply alpha_exp_eval. eapply H0; eauto. eauto.
-    - provide_invariants_3; simpl in *. no_step. simpl in *.
+    + provide_invariants_3; simpl in *. no_step. simpl in *.
       get_functional; subst. simpl in *. congruence.
       get_functional; subst. simpl in *. congruence.
-    - no_step; eauto. provide_invariants_3; eauto.
-  + case_eq (exp_eval E e); intros.
+    + no_step; eauto. provide_invariants_3; eauto.
+  - case_eq (exp_eval E e); intros.
     one_step. erewrite <- alpha_exp_eval; eauto.
     eapply alphaSim_sim; econstructor; eauto using envCorr_update.
-    eapply simE; try eapply star_refl; eauto; stuck.
+    no_step.
     erewrite alpha_exp_eval in H1; eauto. congruence.
-  + case_eq (exp_eval E e); intros.
+  - case_eq (exp_eval E e); intros.
     case_eq (val2bool v); intros.
     one_step. erewrite <- alpha_exp_eval; eauto.
     eapply alphaSim_sim; econstructor; eauto.
@@ -301,7 +350,19 @@ Proof.
     eapply alphaSim_sim; econstructor; eauto.
     no_step; erewrite <- alpha_exp_eval in def; eauto.
     congruence. congruence.
-  + one_step. eapply alphaSim_sim.
+  - remember (omap (exp_eval E) Y); intros. symmetry in Heqo.
+    assert (omap (exp_eval E') Y' = o).
+    erewrite omap_agree_2; eauto.
+    intros. symmetry.
+    eapply alpha_exp_eval. eapply H0; eauto; eauto. hnf; intros; eauto.
+    destruct o.
+    extern_step.
+    + eexists; split. econstructor; eauto. congruence.
+      eapply alphaSim_sim; econstructor; eauto using envCorr_update.
+    + eexists; split. econstructor; eauto. congruence.
+      eapply alphaSim_sim; econstructor; eauto using envCorr_update.
+    + no_step.
+  - one_step. eapply alphaSim_sim.
     econstructor; eauto.
     econstructor; eauto. econstructor; eauto.
 Qed.
