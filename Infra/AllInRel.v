e0a9d7e25d676837c5b0f4f1dd340d3427edc9f5
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-18 15:29:39 +0200

    Fix AIR5 and add similar AIR53 relation.

--- Infra/AllInRel.v
+++ Infra/AllInRel.v
@@ -8,8 +8,8 @@ Set Implicit Arguments.
 Section AIR2.
   Variable X Y : Type.
   Variable R : list X -> Y -> Prop.
-  
-  Inductive AIR2 
+
+  Inductive AIR2
     : list X -> list Y -> Prop :=
   | AIR2_nil : AIR2 nil nil
   | AIR2_cons x XL y (pf:R (x::XL) y)
@@ -17,8 +17,8 @@ Section AIR2.
     AIR2 XL YL ->
     AIR2 (x::XL) (y::YL).
 
-  Lemma AIR2_nth LT L l blkt : 
-    AIR2 LT L 
+  Lemma AIR2_nth LT L l blkt :
+    AIR2 LT L
     -> get LT l blkt
     -> exists blk:Y,
       get L l blk /\ R (drop l LT) blk.
@@ -29,20 +29,20 @@ Section AIR2.
     eexists; repeat split; eauto using get.
   Qed.
 
-  Lemma AIR2_drop LT L n 
+  Lemma AIR2_drop LT L n
     : AIR2 LT L -> AIR2 (drop n LT) (drop n L).
   Proof.
     general induction n; simpl; eauto.
     destruct L; inv H; simpl; eauto using AIR2.
   Qed.
-  
+
 End AIR2.
 
 
-Ltac provide_invariants_2 := 
-match goal with 
+Ltac provide_invariants_2 :=
+match goal with
   | [ H : AIR2 ?R ?A ?B, H' : get ?A ?n ?b |- _ ] =>
-    let X := fresh H in 
+    let X := fresh H in
     destruct (AIR2_nth H H') as [? [? X]]; eauto; inv X;
     repeat get_functional; (try subst);
     let X'' := fresh H in pose proof (AIR2_drop n H) as X'';
@@ -58,8 +58,8 @@ end.
 Section AIR3.
   Variable X Y Z : Type.
   Variable R : list X -> Y -> Z -> Prop.
-  
-  Inductive AIR3 
+
+  Inductive AIR3
     : list X -> list Y -> list Z -> Prop :=
   | AIR3_nil : AIR3 nil nil nil
   | AIR3_cons x XL y z (pf:R (x::XL) y z)
@@ -67,8 +67,8 @@ Section AIR3.
     AIR3 XL YL ZL ->
     AIR3 (x::XL) (y::YL) (z::ZL).
 
-  Lemma AIR3_nth LT L L' l blkt : 
-    AIR3 LT L L' 
+  Lemma AIR3_nth LT L L' l blkt :
+    AIR3 LT L L'
     -> get LT l blkt
     -> exists blk:Y, exists blk':Z,
       get L l blk /\ get L' l blk' /\ R (drop l LT) blk blk'.
@@ -80,44 +80,44 @@ Section AIR3.
     do 2 eexists; repeat split; eauto using get.
   Qed.
 
-  Lemma AIR3_nth2 LT L L' l blk : 
-    AIR3 LT L L' 
+  Lemma AIR3_nth2 LT L L' l blk :
+    AIR3 LT L L'
     -> get L l blk
     -> exists blkt, exists blk':Z,
       get LT l blkt /\ get L' l blk' /\ R (drop l LT) blk blk'.
   Proof.
     intros. general induction H; isabsurd.
-    inv H0. eauto using get. 
+    inv H0. eauto using get.
     edestruct IHAIR3 as [blkt [blk' [A [B C]]]]; eauto.
     eauto 20 using get.
   Qed.
 
-  Lemma AIR3_nth3 LT L L' l blk : 
-    AIR3 LT L L' 
+  Lemma AIR3_nth3 LT L L' l blk :
+    AIR3 LT L L'
     -> get L' l blk
     -> exists blkt, exists blk':Y,
                       get LT l blkt /\ get L l blk' /\ R (drop l LT) blk' blk.
   Proof.
     intros. general induction H; isabsurd.
-    inv H0. eauto using get. 
+    inv H0. eauto using get.
     edestruct IHAIR3 as [blkt [blk' [A [B C]]]]; eauto.
     eauto 20 using get.
   Qed.
 
-  Lemma AIR3_drop LT L L' n 
+  Lemma AIR3_drop LT L L' n
     : AIR3 LT L L' -> AIR3 (drop n LT) (drop n L) (drop n L').
   Proof.
     general induction n; simpl; eauto.
     destruct L,L'; inv H; simpl; eauto using AIR3.
   Qed.
-  
+
 End AIR3.
 
 
-Ltac provide_invariants_3 := 
-match goal with 
+Ltac provide_invariants_3 :=
+match goal with
   | [ H : AIR3 ?R ?A ?B ?C, H' : get ?A ?n ?b |- _ ] =>
-    let X := fresh H in 
+    let X := fresh H in
     destruct (AIR3_nth H H') as [? [? [? [? X]]]]; try eassumption; inversion X; try subst;
     repeat get_functional; (try subst);
     let X'' := fresh H in pose proof (AIR3_drop n H) as X'';
@@ -126,22 +126,22 @@ match goal with
          (try rewrite <- H'' in X'');
           let X' := fresh H in
          pose proof (get_drop_eq H' H'') as X'; inversion X'; try subst; try clear X'
-    end 
+    end
   | [ H : AIR3 ?R ?A ?B ?C, H' : get ?B ?n ?b |- _ ] =>
-    match goal with 
+    match goal with
       | [ H'' : get A ?n ?b |- _ ] => fail 1
       | _ => destruct (AIR3_nth2 H H') as [? [? [? [? ?]]]]; dcr; provide_invariants_3
     end
   | [ H : AIR3 ?R ?A ?B ?C, H' : get ?C ?n ?b |- _ ] =>
-    match goal with 
+    match goal with
       | [ H'' : get A ?n ?b |- _ ] => fail 1
       | _ => destruct (AIR3_nth3 H H') as [? [? [? [? ?]]]]; dcr; provide_invariants_3
     end
 end.
 (*
-match goal with 
+match goal with
   | [ H : AIR3 ?R ?A ?B ?C, H' : get ?A ?n ?b |- _ ] =>
-    let X := fresh H in 
+    let X := fresh H in
     destruct (AIR3_nth H H') as [? [? [? [? X]]]]; try eassumption; inversion X; try subst;
     repeat get_functional; (try subst);
     let X'' := fresh H in pose proof (AIR3_drop n H) as X'';
@@ -152,7 +152,7 @@ match goal with
           pose proof (get_drop_eq H' H'') as X'; inversion X'; try subst; try clear X'
     end
   | [ H : AIR3 ?R ?A ?B ?C, H' : get ?B ?n ?b |- _ ] =>
-    match goal with 
+    match goal with
       | [ H'' : get A ?n ?b |- _ ] => fail 1
       | _ => destruct (AIR3_nth2 H H') as [? [? [? [? ?]]]]; dcr; provide_invariants_3
     end
@@ -164,8 +164,8 @@ end.
 Section AIR4.
   Variable W X Y Z : Type.
   Variable R : list W -> list X -> Y -> Z -> Prop.
-  
-  Inductive AIR4 
+
+  Inductive AIR4
     : list W -> list X -> list Y -> list Z -> Prop :=
   | AIR4_nil : AIR4 nil nil nil nil
   | AIR4_cons (w:W) (WL:list W) x XL y z (pf:R (w::WL) (x::XL) y z)
@@ -173,22 +173,22 @@ Section AIR4.
     AIR4 WL XL YL ZL ->
     AIR4 (w::WL) (x::XL) (y::YL) (z::ZL).
 
-  Lemma AIR4_nth WL LT L L' l blkw blkt : 
-    AIR4 WL LT L L' 
+  Lemma AIR4_nth WL LT L L' l blkw blkt :
+    AIR4 WL LT L L'
     -> get WL l blkw
     -> get LT l blkt
     -> exists blk:Y, exists blk':Z,
       get L l blk /\ get L' l blk' /\ R (drop l WL) (drop l LT) blk blk'.
   Proof.
     intros. general induction H; isabsurd.
-    inv H0. inv H1. eexists. eexists. 
+    inv H0. inv H1. eexists. eexists.
     split; eauto using get.
     inv H1.
     edestruct IHAIR4 as [blk [blk' [A [B C]]]]; eauto.
     eexists; eexists; repeat split; eauto using get.
   Qed.
 
-  Lemma AIR4_drop WL LT L L' n 
+  Lemma AIR4_drop WL LT L L' n
     : AIR4 WL LT L L' -> AIR4 (drop n WL) (drop n LT) (drop n L) (drop n L').
   Proof.
     general induction n; simpl; eauto.
@@ -203,8 +203,8 @@ Section AIR4.
 End AIR4.
 
 
-Lemma AIR4_nth' W X Y Z (R:list W -> list X -> Y -> Z -> Prop) WL LT L L' l blkw : 
-  AIR4 R WL LT L L' 
+Lemma AIR4_nth' W X Y Z (R:list W -> list X -> Y -> Z -> Prop) WL LT L L' l blkw :
+  AIR4 R WL LT L L'
   -> get WL l blkw
   -> exists blkt, exists blk:Y, exists blk':Z,
    get LT l blkt /\ get L l blk /\ get L' l blk' /\ R (drop l WL) (drop l LT) blk blk'.
@@ -215,8 +215,8 @@ Proof.
   do 4 eexists; intuition (eauto using get).
 Qed.
 
-Lemma AIR4_nth_2 W X Y Z (R:list W -> list X -> Y -> Z -> Prop) WL LT L L' l blkw : 
-  AIR4 R WL LT L L' 
+Lemma AIR4_nth_2 W X Y Z (R:list W -> list X -> Y -> Z -> Prop) WL LT L L' l blkw :
+  AIR4 R WL LT L L'
   -> get LT l blkw
   -> exists blkt, exists blk:Y, exists blk':Z,
    get WL l blkt /\ get L l blk /\ get L' l blk' /\ R (drop l WL) (drop l LT) blk blk'.
@@ -228,9 +228,9 @@ Proof.
 Qed.
 
 Ltac provide_invariants_4 :=
-    match goal with 
-      | [ H : AIR4 ?R ?A ?A' ?B ?C, H' : get ?A ?n ?b, I : get ?A' ?n ?b' |- _ ] => 
-        let X := fresh H in 
+    match goal with
+      | [ H : AIR4 ?R ?A ?A' ?B ?C, H' : get ?A ?n ?b, I : get ?A' ?n ?b' |- _ ] =>
+        let X := fresh H in
         destruct (AIR4_nth H H' I) as [? [? [? [? X]]]]; eauto; inv X;
         repeat get_functional; (try subst);
         let X'' := fresh H in pose proof (AIR4_drop n H) as X'';
@@ -239,43 +239,43 @@ Ltac provide_invariants_4 :=
                                  (try rewrite <- H'' in X'');
                                    let X' := fresh H in
                                    pose proof (get_drop_eq H' H'') as X'; inv X'; try clear X'
-                             end 
+                             end
     end.
 
 
 Section AIR5.
   Variable U W X Y Z : Type.
   Variable R : list U -> list W -> X -> Y -> Z -> Prop.
-  
-  Inductive AIR5 
+
+  Inductive AIR5
     : list U -> list W -> list X -> list Y -> list Z -> Prop :=
   | AIR5_nil : AIR5 nil nil nil nil nil
-  | AIR5_cons (u:U) (UL:list U) (w:W) (WL:list W) x XL y z 
+  | AIR5_cons (u:U) (UL:list U) (w:W) (WL:list W) x XL y z
               (pf:R (u::UL) (w::WL) x y z)
               (YL:list Y) (ZL:list Z) :
       AIR5 UL WL XL YL ZL ->
       AIR5 (u::UL) (w::WL) (x::XL) (y::YL) (z::ZL).
 
-  Lemma AIR5_nth UL WL LT L L' l blkw blkt : 
-    AIR5 UL WL LT L L' 
+  Lemma AIR5_nth UL WL LT L L' l blkw blkt :
+    AIR5 UL WL LT L L'
     -> get UL l blkw
     -> get WL l blkt
-    -> exists lt:X, exists blk:Y, exists blk':Z, 
+    -> exists lt:X, exists blk:Y, exists blk':Z,
       get LT l lt /\ get L l blk /\ get L' l blk'
       /\ R (drop l UL) (drop l WL) lt blk blk'.
   Proof.
     intros. general induction H; isabsurd.
-    inv H0. inv H1. do 3 eexists. 
+    inv H0. inv H1. do 3 eexists.
     split; eauto using get.
     inv H1.
     edestruct IHAIR5 as [lt' [blk [blk' [A [B [C D]]]]]]; eauto.
     do 3 eexists; repeat split; eauto using get.
   Qed.
 
-  Lemma AIR5_nth2 UL WL LT L L' l lt : 
-    AIR5 UL WL LT L L' 
+  Lemma AIR5_nth2 UL WL LT L L' l lt :
+    AIR5 UL WL LT L L'
     -> get LT l lt
-    -> exists (blkw:U) (blkt:W) (blk:Y), exists (blk':Z), 
+    -> exists (blkw:U) (blkt:W) (blk:Y), exists (blk':Z),
       get UL l blkw /\ get WL l blkt /\ get L l blk /\ get L' l blk'
       /\ R (drop l UL) (drop l WL) lt blk blk'.
   Proof.
@@ -285,8 +285,8 @@ Section AIR5.
     do 4 eexists; repeat split; eauto using get.
   Qed.
 
-  Lemma AIR5_drop UL WL LT L L' n 
-    : AIR5 UL WL LT L L' 
+  Lemma AIR5_drop UL WL LT L L' n
+    : AIR5 UL WL LT L L'
       -> AIR5 (drop n UL) (drop n WL) (drop n LT) (drop n L) (drop n L').
   Proof.
     general induction n; simpl; eauto.
@@ -294,22 +294,115 @@ Section AIR5.
   Qed.
 
   Lemma AIR5_length UL WL LT L L'
-        : AIR5 UL WL LT L L' 
+        : AIR5 UL WL LT L L'
           -> length UL = length WL
-          /\ length WL = length LT 
-          /\ length LT = length L 
+          /\ length WL = length LT
+          /\ length LT = length L
           /\ length L = length L'.
   Proof.
     intros. general induction H; eauto; simpl; dcr; repeat split; congruence.
   Qed.
 End AIR5.
 
+Ltac provide_invariants_5 :=
+  match goal with
+    | [ H : AIR5 ?R ?A ?A' ?A'' ?B ?C, H' : get ?A ?n ?b, I : get ?A' ?n ?b' |- _ ] =>
+      let X := fresh H in
+      destruct (AIR5_nth H H' I) as [? [? [? [? [? [? X]]]]]]; eauto; inv X;
+      repeat get_functional; (try subst);
+      let X'' := fresh H in pose proof (AIR5_drop n H) as X'';
+                          match goal with
+                            | [ H'' : ?x :: ?DL = drop ?n ?Lv |- _ ] =>
+                              (try rewrite <- H'' in X'');
+                                let X' := fresh H in
+                                pose proof (get_drop_eq H' H'') as X'; inv X'; try clear X'
+                          end
+  end.
+
+Section AIR53.
+  Variable U W X Y Z : Type.
+  Variable R : list U -> list W -> list X -> Y -> Z -> Prop.
+
+  Inductive AIR53
+    : list U -> list W -> list X -> list Y -> list Z -> Prop :=
+  | AIR53_nil : AIR53 nil nil nil nil nil
+  | AIR53_cons (u:U) (UL:list U) (w:W) (WL:list W) x XL y z
+              (pf:R (u::UL) (w::WL) (x::XL) y z)
+              (YL:list Y) (ZL:list Z) :
+      AIR53 UL WL XL YL ZL ->
+      AIR53 (u::UL) (w::WL) (x::XL) (y::YL) (z::ZL).
+
+  Lemma AIR53_nth UL WL LT L L' l blkw blkt :
+    AIR53 UL WL LT L L'
+    -> get UL l blkw
+    -> get WL l blkt
+    -> exists lt:X, exists blk:Y, exists blk':Z,
+      get LT l lt /\ get L l blk /\ get L' l blk'
+      /\ R (drop l UL) (drop l WL) (drop l LT) blk blk'.
+  Proof.
+    intros. general induction H; isabsurd.
+    inv H0. inv H1. do 3 eexists.
+    split; eauto using get.
+    inv H1.
+    edestruct IHAIR53 as [lt' [blk [blk' [A [B [C D]]]]]]; eauto.
+    do 3 eexists; repeat split; eauto using get.
+  Qed.
+
+  Lemma AIR53_nth2 UL WL LT L L' l lt :
+    AIR53 UL WL LT L L'
+    -> get LT l lt
+    -> exists (blkw:U) (blkt:W) (blk:Y), exists (blk':Z),
+      get UL l blkw /\ get WL l blkt /\ get L l blk /\ get L' l blk'
+      /\ R (drop l UL) (drop l WL) (drop l LT) blk blk'.
+  Proof.
+    intros. general induction H; isabsurd.
+    inv H0. do 4 eexists; eauto using get.
+    edestruct IHAIR53 as [lt' [blk [blk' [A [B [C D]]]]]]; eauto; dcr.
+    do 4 eexists; repeat split; eauto using get.
+  Qed.
+
+  Lemma AIR53_drop UL WL LT L L' n
+    : AIR53 UL WL LT L L'
+      -> AIR53 (drop n UL) (drop n WL) (drop n LT) (drop n L) (drop n L').
+  Proof.
+    general induction n; simpl; eauto.
+    destruct L,L'; inv H; simpl; eauto.
+  Qed.
+
+  Lemma AIR53_length UL WL LT L L'
+        : AIR53 UL WL LT L L'
+          -> length UL = length WL
+          /\ length WL = length LT
+          /\ length LT = length L
+          /\ length L = length L'.
+  Proof.
+    intros. general induction H; eauto; simpl; dcr; repeat split; congruence.
+  Qed.
+End AIR53.
+
+Ltac provide_invariants_53 :=
+  match goal with
+    | [ H : AIR53 ?R ?A ?A' ?A'' ?B ?C,
+        H' : get ?A ?n ?b,
+        I : get ?A' ?n ?b' |- _ ] =>
+      let X := fresh H in
+      destruct (AIR53_nth H H' I) as [? [? [? [? [? [? X]]]]]]; eauto; inv X;
+      repeat get_functional; (try subst);
+      let X'' := fresh H in pose proof (AIR53_drop n H) as X'';
+                          match goal with
+                            | [ H'' : ?x :: ?DL = drop ?n ?Lv |- _ ] =>
+                              (try rewrite <- H'' in X'');
+                                let X' := fresh H in
+                                pose proof (get_drop_eq H' H'') as X'; inv X'; try clear X'
+                          end
+  end.
+
 
 Section PIR2.
   Variable X Y : Type.
   Variable R : X -> Y -> Prop.
-  
-  Inductive PIR2 
+
+  Inductive PIR2
     : list X -> list Y -> Prop :=
   | PIR2_nil : PIR2 nil nil
   | PIR2_cons x XL y (pf:R x y)
@@ -317,7 +410,7 @@ Section PIR2.
     PIR2 XL YL ->
     PIR2 (x::XL) (y::YL).
 
-  Lemma PIR2_nth (L:list X) (L':list Y) l blk : 
+  Lemma PIR2_nth (L:list X) (L':list Y) l blk :
     PIR2 L L'
     -> get L l blk
     -> exists blk':Y,
@@ -329,7 +422,7 @@ Section PIR2.
     eexists; repeat split; eauto using get.
   Qed.
 
-  Lemma PIR2_nth_2 (L:list X) (L':list Y) l blk : 
+  Lemma PIR2_nth_2 (L:list X) (L':list Y) l blk :
     PIR2 L L'
     -> get L' l blk
     -> exists blk',
@@ -342,7 +435,7 @@ Section PIR2.
   Qed.
 
 
-  Lemma PIR2_drop LT L n 
+  Lemma PIR2_drop LT L n
     : PIR2 LT L -> PIR2 (drop n LT) (drop n L).
   Proof.
     general induction n; simpl; eauto.
@@ -374,7 +467,7 @@ Proof.
   hnf; intros. general induction x; eauto using PIR2.
 Qed.
 
-Instance PIR2_trans {X} (R:X -> X -> Prop) `{Transitive _ R} 
+Instance PIR2_trans {X} (R:X -> X -> Prop) `{Transitive _ R}
 : Transitive (PIR2 R).
 Proof.
   hnf; intros.
@@ -385,16 +478,16 @@ Proof.
 Qed.
 
 
-Ltac provide_invariants_P2 := 
-match goal with 
+Ltac provide_invariants_P2 :=
+match goal with
   | [ H : PIR2 ?R ?A ?B, H' : get ?A ?n ?b |- _ ] =>
-    let X := fresh H in 
+    let X := fresh H in
     destruct (PIR2_nth H H') as [? [? X]]; eauto; inv X;
     repeat get_functional; (try subst) ;
     let X'' := fresh H in pose proof (PIR2_drop n H) as X''
 end.
 
-(* 
+(*
 *** Local Variables: ***
 *** coq-load-path: (("../" "Lvc")) ***
 *** End: ***
