709b59d52d0094c365f62fa6af88d6ca362fbc7b
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-21 21:03:51 +0200

    Fix backwards analysis for external calls.

--- AbsInt.v
+++ AbsInt.v
@@ -4,14 +4,14 @@ Require Import Plus Util AllInRel Map.
 Require Import Val Var Env EnvTy IL Annotation Lattice DecSolve LengthEq MoreList.
 
 Class AbstractInterpretation Dom FunDom `{SemiLattice Dom} := {
-  transform : list FunDom -> stmt -> ann Dom -> Dom;                                  
+  transform : list FunDom -> stmt -> ann Dom -> Dom;
   mkFunDom : params -> ann Dom -> FunDom
 }.
 
-Fixpoint backward {Dom} {FunDom} `{SemiLattice Dom} (AI:AbstractInterpretation Dom (FunDom)) 
+Fixpoint backward {Dom} {FunDom} `{SemiLattice Dom} (AI:AbstractInterpretation Dom (FunDom))
          (st:stmt) (AL:list FunDom) (a:ann Dom) {struct st} : ann Dom :=
   match st, a with
-    | stmtExp x e s as st, ann1 d ans => 
+    | stmtExp x e s as st, ann1 d ans =>
       let ans' := backward AI s AL ans in
       let d' := ann1 d ans' in
       ann1 (transform AL st d') ans'
@@ -20,37 +20,27 @@ Fixpoint backward {Dom} {FunDom} `{SemiLattice Dom} (AI:AbstractInterpretation D
       let ant' := backward AI t AL ant in
       let an' := (ann2 d ans' ant') in
       ann2 (transform AL st an') ans' ant'
-      
-    | stmtGoto f Y as st, ann0 d as an => 
+
+    | stmtGoto f Y as st, ann0 d as an =>
       ann0 (transform AL st an)
 
-    | stmtReturn x as st, ann0 d as an => 
+    | stmtReturn x as st, ann0 d as an =>
       ann0 (transform AL st an)
 
-    | stmtLet Z s t as st, ann2 d ans ant => 
+    | stmtExtern x f Y s as st, ann1 d ans =>
+      let ans' := backward AI s AL ans in
+      let d' := ann1 d ans' in
+      ann1 (transform AL st d') ans'
+
+    | stmtLet Z s t as st, ann2 d ans ant =>
       let ans' := backward AI s (mkFunDom Z ans::AL) ans in
       let ant' := backward AI t (mkFunDom Z ans'::AL) ant in
       let d' := ann2 d ans' ant' in
       ann2 (transform (mkFunDom Z ans'::AL)
-                        st 
+                        st
                         d') ans' ant'
     | _, an => an
   end.
-
-
-(*
-Instance eq_dec_ann A (OrderedType A _eq) : EqDec (ann A) _eq.
-Proof.
-hnf; intros.
-general induction x; destruct y; try now (right; discriminate).
-+ destruct (a == a0); edestruct IHx with (y:=y); hnf in *; try subst; eauto; try dec_solve.
-+ destruct (a == a0); edestruct IHx1 with (y:=y1); edestruct IHx2 with (y:=y2); hnf in *; try subst; eauto; try dec_solve.
-+ destruct (a == a0); hnf in *; try subst; eauto; try dec_solve.
-+ destruct (a == a0); edestruct IHx with (y:=y); hnf in *; try subst; eauto; try dec_solve.
-+ destruct (a == a0); hnf in *; try subst; eauto; try dec_solve.
-+ destruct (a == a0); edestruct IHx1 with (y:=y1); edestruct IHx2 with (y:=y2); hnf in *; try subst; eauto; try dec_solve.
-Defined.
-*)
 
 Set Implicit Arguments.
 Section Analysis.
@@ -64,17 +54,16 @@ Section Analysis.
   Context `{AI:AbstractInterpretation Dom}.
 
   Definition step s d :=
-    let d' := backward AI s nil d in 
+    let d' := backward AI s nil d in
     (d', if [ann_R lt d' d] then false else true).
 
-  Definition analysis (s:stmt) := 
+  Definition analysis (s:stmt) :=
     first (setAnn s bottom) (step s).
 End Analysis.
 
 
-(* 
+(*
 *** Local Variables: ***
 *** coq-load-path: (("." "Lvc")) ***
 *** End: ***
 *)
-
