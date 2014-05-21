9dcbd5ac3f7b40641a0b9ea815f79f4914f0593a
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-05-21 21:04:11 +0200

    Fix liveness analysis for external calls.

--- LivenessAnalysis.v
+++ LivenessAnalysis.v
@@ -14,15 +14,17 @@ Defined.
 
 Definition liveness_transform (DL:list (set var * params)) st a :=
   match st, a with
-    | stmtExp x e s as st, ann1 d ans => 
+    | stmtExp x e s as st, ann1 d ans =>
       (getAnn ans \ {{x}}) ∪ (if [x ∈ getAnn ans] then Exp.freeVars e else ∅)
     | stmtIf e s t as st, ann2 d ans ant =>
       Exp.freeVars e ∪ getAnn ans ∪ getAnn ant
-    | stmtGoto f Y as st, ann0 d as an => 
+    | stmtGoto f Y as st, ann0 d as an =>
       let (lv,Z) := nth (counted f) DL (∅,nil) in
       lv \ of_list Z ∪ list_union (List.map Exp.freeVars (filter_by (fun x => B[x ∈ lv]) Z Y))
     | stmtReturn e as st, ann0 d as an => Exp.freeVars e
-    | stmtLet Z s t as st, ann2 d ans ant => 
+    | stmtExtern x f Y s as st, ann1 d ans as an =>
+      (getAnn ans \ {{x}}) ∪ list_union (List.map Exp.freeVars Y)
+    | stmtLet Z s t as st, ann2 d ans ant =>
        (getAnn ans \ of_list Z) ∪ getAnn ant
     | _, an => ∅
   end.
@@ -34,7 +36,7 @@ Instance liveness_analysis : AbstractInterpretation (set var) (set var * params)
 }.
 
 
-(* 
+(*
 *** Local Variables: ***
 *** coq-load-path: (("." "Lvc")) ***
 *** End: ***
