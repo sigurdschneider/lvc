65d87cb1986968660cb7e390b751f6fbc9bdaa23
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2014-06-09 11:40:05 +0200

    Check for ruby 1.9 (not optimal, as it does not permit ruby 2.0).

--- configure.sh
+++ configure.sh
@@ -18,14 +18,20 @@ while [ $# != 0 ]; do
   shift
 done
 
+if ! [[ $(ruby -v) =~ ^ruby\ 1.9 ]]; then
+	echo "Ruby 1.9 not in path, defaulting to vanilla"
+	VANILLA=yes
+fi
 
 SOURCES=`find -type f -iname '*.v' -printf '%P\n'`
 coq_makefile -R . Lvc extraction $SOURCES > Makefile
 echo "Makefile generated."
 
+echo "Patching Makefile to include target 'extraction'."
+sed -i '/.\/extraction:/c\.\/extraction: Compiler.vo' Makefile
+
 if [ -z "$VANILLA" ]; then
 	echo "Patching Makefile to use ruby-based timing scripts (use --vanilla if undesired)."
-  sed -i '/.\/extraction:/c\.\/extraction: Compiler.vo' Makefile
   sed -i 's/$(COQC) $(COQDEBUG) $(COQFLAGS)/@.\/time.sh $(if $(findstring j,$(MAKEFLAGS)),--parallel,) $(COQC) $(COQDEBUG) $(COQFLAGS)/' Makefile
 fi
 
