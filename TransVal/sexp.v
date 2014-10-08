bbaaa6ea4a09fb0610bd9c1218462840217039c7
Heiko Becker <heikobecker92@gmail.com>
2014-10-08 15:44:27 +0200

    Make smt expressions use twos complement arithmetics

--- TransVal/sexp.v
+++ TransVal/sexp.v
@@ -1,4 +1,3 @@
-
 Require Import List NPeano Decidable.
 Require Import IL.
 
@@ -17,19 +16,196 @@ recursive function definitions.
 **)
 Definition bitvec := list bit.
 
+(** Define the length of a bitvector k **)
+Definition k:= 32.
+
 Definition senv:Type := var -> option bitvec.
 
-(**
-Define SMT expressions on bitvectors. For the moment I only add +,-_2,*,/constants and variables
-**)
+(** Define SMT expressions on bitvectors. **)
 Inductive sexp :=
-|plus: sexp -> sexp -> sexp
-|sub: sexp -> sexp -> sexp
-|mult: sexp -> sexp -> sexp
-|div: sexp -> sexp -> sexp
+(** a + b **)
+|splus: sexp -> sexp -> sexp
+(** a - b **)
+|ssub: sexp -> sexp -> sexp
+(** a * b **)
+|smult: sexp -> sexp -> sexp
+(** a / b **)
+|sdiv: sexp -> sexp -> sexp
+(** a == b **)
+|seq: sexp -> sexp -> sexp
+(** ! a **)
 |sneg: sexp->sexp
-|const: bitvec -> sexp
+(** constants **)
+|sconst: bitvec -> sexp
+(** variables **)
 |svar: var -> sexp.
+
+(** HELPER FUNCTIONS for definition of evaluation functions **)
+
+(** zero extension **)
+Fixpoint zext (k:nat) (bv:bitvec) :=
+match k with
+| 0 => nil
+| S k' => match bv with
+              |nil => O:: zext k' nil
+              | a::b' => a::zext k' b'
+          end
+end.
+
+(** sign extension **)
+Fixpoint sext (k:nat) (bv:bitvec) (b:bit) :=
+match k with
+| 0 => nil
+| S k' => match bv with
+              | nil => b:: sext k' nil b
+              | a::b' => a::sext k' b' a
+          end
+end.
+
+Definition bitComplement b :=
+match b with
+| I => O
+| O => I
+end.
+
+(* Complement *)
+Fixpoint complement' k  b :=
+match k with
+| 0 => nil
+| S k' => match sext k b O with
+              | nil => nil
+              | a::b' => bitComplement  a :: complement' k' b'
+          end
+end.
+
+Definition complement b :=
+complement' k b.
+
+(** Left shift **)
+Fixpoint shl s b :=
+match s with 
+|0 => b
+| S s' => O::b
+end.
+
+(** Increment function based on the size of an argument**)
+Fixpoint incr' (k:nat) (b:bitvec) :=
+match k with
+| 0 => nil
+| S k' => match b with
+            | nil => I::(sext k' nil I)
+            | O::b' => I:: (sext k' b' I)
+            | I::b' => O:: incr' k' b'
+          end 
+end.
+
+(** increment function that uses the defined size **)
+Definition incr (b:bitvec) :=
+incr' k (sext k b O).
+
+(** Decrement function TODO: Maybe remove it **)
+Fixpoint decr (b:bitvec) :=
+match b with
+| nil => nil
+| O::nil => O::nil
+| I::b' => O::b'
+| O::b' => I::decr b'
+end.
+
+Definition bitAnd (b1:bit) (b2:bit) :=
+match b1, b2 with
+|I,I => I
+|_,_ => O
+end.
+
+Definition bitOr (b1:bit) (b2:bit) :=
+match b1, b2 with
+|O,O => O
+|_,_ => I
+end.
+
+Definition bitXor (b1:bit) (b2:bit) :=
+match b1, b2 with
+|O,O => O
+|I,I => O
+|_,_ => I
+end.
+
+Fixpoint bvAndBit k bv b :bitvec :=
+match k with
+| 0 => nil
+| S k' => match bv with
+              |b1:: bv' => (bitAnd b1 b) :: (bvAndBit k' bv' b)
+              |nil => O::bvAndBit k' nil b
+          end
+end.
+
+(* bounded addition for bitvectors such that we get the 2's complement semantics **)
+Fixpoint bvAddBounded k a b c :=
+match k with
+| 0 => nil
+| S k'=> match a with
+             |a1::a' => match b with
+                            |b1::b' 
+                             => let c' := (bitOr (bitAnd b1 c)(bitOr (bitAnd a1 c)(bitAnd a1 b1))) in 
+                                let r  := (bitXor a1 (bitXor b1 c)) in
+                                r ::(bvAddBounded k' a' b' c' )
+                            |nil 
+                             => let r := bitXor a1 c in
+                                 let c':= bitAnd a1 c in
+                                 r:: (bvAddBounded k' a' nil c')
+                        end
+             |nil => match b with
+                         |b1::b' 
+                          => let r := bitXor b1 c in
+                             let c' := bitAnd b1 c in
+                             r:: (bvAddBounded k' nil b' c')
+                          |nil => c::(bvAddBounded k' nil nil c)
+                     end
+         end
+end.
+
+(** Bitvector Addition for binary  numbers. Forwards the call to bvAddBounded with k defined before. **)
+Definition bvAdd (a:bitvec) (b:bitvec)  :bitvec :=
+bvAddBounded k (sext k a O) (sext k b O) O.
+
+Definition bvSub (a:bitvec) (b:bitvec) :bitvec :=
+bvAdd( bvAdd a (complement b) ) (zext k (I::nil)).
+
+Fixpoint bvMult' k s a b :=
+match k with
+| 0 => nil
+| S k' => match b with
+            |b1::b' => bvAddBounded (2*k) (shl s (bvAndBit (k+s) a b1)) (bvMult' k' (S s) a b' ) O
+            |nil => nil
+                    end
+end.
+
+
+Definition bvMult a b :=
+bvMult' (2*k) 0 (sext (2*k) a O) (sext (2*k) b O).
+
+Definition neg b :=
+bvAdd (complement b) (zext k (I::nil)).
+
+Fixpoint bvEq b1 b2 :=
+match b1, b2 with
+| nil, nil => sext k (nil) I
+| nil, _ => sext k nil O
+| _, nil => sext k nil O
+| a1::b1', a2::b2' => match bitAnd a1 a2 with
+                        |I => bvEq b1' b2'
+                        |O => sext k (nil) O
+                      end
+
+end.
+
+Fixpoint toBool b :=
+match b with
+| nil => false
+| I::b' => true
+| O::b' => toBool b'
+end.
 
 (** Define a function to map a single bit to a natural number **)
 Definition bitToNat (b1:bit) :nat :=
@@ -65,16 +241,69 @@ intros. general induction b.
 - destruct IHb.  destruct a.
   + exists (2*x).  simpl. rewrite H. omega.
   + exists ((2*x)+1). simpl. rewrite H. omega. 
-Qed.
+Qed. 
 
-(** Increment function which should be equal to the addition of 1 **)
-Fixpoint incr (b:bitvec) :=
-match b with
-| nil => I::nil
-| O::b' => I::b'
-| I::b' => O:: incr b'
-end .
+Definition min a b :=
+a-(a-b).
 
+(** TODO: Find better division algorithm **)
+(** a / b = None, b = 0, Some ( S (a -b)/b) **)
+Fixpoint bvDiv' (a:nat) (b:nat) (akk:bitvec) :=
+match a with
+| 0 => Some akk
+| S n' => match b with
+           | 0 => None
+           | _ => bvDiv' (min  n' (a - b)) b (incr akk)
+          end
+end.
+
+Definition bvDiv (b1:bitvec) (b2:bitvec) :option bitvec:=
+bvDiv' (bitvecToNat b1) (bitvecToNat b2) (O::nil).
+
+Definition evalOn (o1:option bitvec) (o2:option bitvec) (f:bitvec->bitvec->option bitvec):=
+match o1, o2 with
+|Some v1, Some v2 =>  f v1 v2
+| _, _ => None
+end.
+
+(**
+evalSexp evaluates an SMT expression given an environment with the variable bindings already 
+introduced and the expression to evaluate. It's returntype is a bitvector option as it is
+possible that
+a) a variable occurs free
+b) a variable has not been defined
+c) evaluation can become stuck
+**)
+
+Fixpoint evalSexp (E: senv)(s:sexp):option bitvec :=
+match s with
+|splus a b 
+  => evalOn ( evalSexp E a) (evalSexp E b) (fun a b => Some (bvAdd a b))
+|ssub a b
+  => evalOn  (evalSexp E a) (evalSexp E b) (fun a b => Some (bvSub a b))
+|smult a b 
+  => evalOn (evalSexp E a) ( evalSexp E b) (fun a b => Some (bvMult a b))
+|sdiv a b  
+  => evalOn ( evalSexp E a) ( evalSexp E b)  bvDiv
+|seq a b
+     => evalOn  (evalSexp E a) ( evalSexp E b) (fun a b => Some (bvEq a b))
+| sneg v
+ => let v' := evalSexp E v in
+    match v' with
+      | Some v => Some (neg v)
+      | None => None
+    end
+| sconst c
+  => Some c
+| svar v
+  => match (E v) with
+         | Some b => Some b
+         | None => None
+     end
+end.
+
+(* Not needed stuff begins here *)
+(*
 Lemma incr_eq_plus_one:
 forall (b:bitvec) (n:nat), bitvecToNat b = n -> bitvecToNat (incr b) = S n.
 
@@ -88,14 +317,18 @@ intros. general induction b.
   + simpl. rewrite (IHb x0 H0) in *. rewrite H0 in *. omega.
 Qed.
 
-Fixpoint decr (b:bitvec) :=
-match b with
-| nil => nil
-| O::nil => O::nil
-| I::b' => O::b'
-| O::b' => I::decr b'
-end.
+(** TODO: Induction case of this proof. Maybe an additional Lemma is needed... **)
+Lemma always_smaller:
+forall n, min n (div2 n) = div2 n.
 
+Proof.
+intros.
+general induction n.
+- reflexivity.
+- simpl. destruct n.
+  + unfold min. omega.
+  +  unfold min. simpl. 
+Qed.
 
 (** Define a min function for the recursion of the natToBitvec function. 
 It must be computed with min = a-(a-b)  **)
@@ -109,21 +342,6 @@ match n with
 |S (0) => 0
 |S (S n') => S(div2 (n'))
 end.
-
-(*
-(** TODO: Induction case of this proof. Maybe an additional Lemma is needed... **)
-Lemma always_smaller:
-forall n, min n (div2 n) = div2 n.
-
-Proof.
-intros.
-general induction n.
-- reflexivity.
-- simpl. destruct n.
-  + unfold min. omega.
-  +  unfold min. simpl. 
-Qed.
-*)
 
 (* TODO: Is a lemma needed that min always evaluates to div2 n? *)
 (** Define a function a function to compute the bitvector for a natural number **)
@@ -154,11 +372,25 @@ intros. destruct n.
     * exists (O:: (natToBitvec (min (S n) (S (div2 n))))). reflexivity.
 Qed.
 
-(*
 Lemma bijective_numbers:
 forall (n:nat) (b:bitvec),
 natToBitvec n = b 
 <-> bitvecToNat b = n.
+
+Definition bvGeq (a:bitvec) (b:bitvec) :bool := 
+let l := bitvecToNat a in
+let r := bitvecToNat b in
+nGeqBool l r.
+
+Lemma bitvec_greaterequal_zero a b:
+b = nil \/ b = O::nil -> bvGeq a b = true.
+
+Proof.
+intros. assert (X:exists n1, bitvecToNat a = n1) by (exact (exists_number_bitvec a)). destruct X. 
+destruct H.
+- unfold bvGeq. rewrite H. rewrite H0. simpl. rewrite number_greaterequal_zero. reflexivity.
+- unfold bvGeq. rewrite H. rewrite H0. simpl. rewrite number_greaterequal_zero. reflexivity. 
+Qed.
 
 Proof.
 intros.
@@ -167,57 +399,6 @@ intros.
   destruct X. rewrite H.
   assert (Y: exists n, bitvecToNat x = n) by (exact (exists_number_bitvec (x))).
   destruct Y.
-*)
-
-Definition bitAnd (b1:bit) (b2:bit) :=
-match b1, b2 with
-|I,I => I
-|_,_ => O
-end.
-
-Definition bitOr (b1:bit) (b2:bit) :=
-match b1, b2 with
-|O,O => O
-|_,_ => I
-end.
-
-Definition bitXor (b1:bit) (b2:bit) :=
-match b1, b2 with
-|O,O => O
-|I,I => O
-|_,_ => I
-end.
-
-Fixpoint bvAdd (a:bitvec) (b:bitvec) (c:bit) :bitvec :=
-match a with
-|a1::a' =>  match b with
-               |b1::b' 
-                  => let c' := (bitOr (bitAnd b1 c)(bitOr (bitAnd a1 c)(bitAnd a1 b1))) in
-                     let r  := (bitXor a1 (bitXor b1 c)) in
-                     r ::(bvAdd a' b' c' )
-               |nil
-                  => let r := bitXor a1 c in
-                     let c':= bitAnd a1 c in
-                       match c' with
-                           |O => r::a'
-                           |I => r::(incr a')
-                       end
-               end
-|nil => match b with
-            |b1::b'
-               => let r := bitXor b1 c in
-                  let c':= bitAnd b1 c in
-                    match c' with
-                        | O => r:: b'
-                        | I => r::(incr b')
-                    end
-            |nil
-               => match c with 
-                      | O => nil
-                      | I => I::nil
-                  end
-       end
-end. 
 
 Lemma bit_bvAdd_lO:
 forall (v:bit) (b:bitvec), bvAdd (v::b) nil O = v::bvAdd b nil O.
@@ -297,7 +478,7 @@ Proof.
 intros. reflexivity.
 Qed.
 
-(* Definition bvAdd b1 b2 :=
+Definition bvAdd b1 b2 :=
 natToBitvec ((bitvecToNat b1) + (bitvecToNat b2)). 
 
 Lemma agree_on_add b1 b2 n1 n2:
@@ -320,7 +501,6 @@ bitvecToNat b1  = n1
 Proof.
 intros. unfold bvSub. rewrite H. rewrite H0. reflexivity.
 Qed.
-*)
 
 Lemma agree_on_add b1 b2 n1 n2 c v:
 bitToNat c = v
@@ -377,121 +557,6 @@ general induction b1.
     { rewrite H. rewrite H0. rewrite (IHb1 b2 x x0 I 1 H2 H H0). omega. }
 Qed.
 
-
-Fixpoint nGeqBool l r :bool :=
-match l, r with
-| 0,0 => true
-| 0,_ => false
-|_, 0 => true
-|S(l),S(r) => nGeqBool (l) (r)
-end.
-
-Lemma number_greaterequal_zero n   :
-nGeqBool n 0 = true.
-
-Proof.
-destruct n; intros; simpl.
-- reflexivity.
-- reflexivity.
-Qed.
-
-Definition bvGeq (a:bitvec) (b:bitvec) :bool := 
-let l := bitvecToNat a in
-let r := bitvecToNat b in
-nGeqBool l r.
-
-Lemma bitvec_greaterequal_zero a b:
-b = nil \/ b = O::nil -> bvGeq a b = true.
-
-Proof.
-intros. assert (X:exists n1, bitvecToNat a = n1) by (exact (exists_number_bitvec a)). destruct X. 
-destruct H.
-- unfold bvGeq. rewrite H. rewrite H0. simpl. rewrite number_greaterequal_zero. reflexivity.
-- unfold bvGeq. rewrite H. rewrite H0. simpl. rewrite number_greaterequal_zero. reflexivity. 
-Qed.
-
-Fixpoint isZero b :=
-match b with
-|nil => true
-|O::nil => true
-|a::b => match a with
-             | I => false
-             | O => isZero b
-         end
-end.
-
-Lemma zero_decidable:
-forall b, decidable(isZero b).
-
-Proof.
-intros.
-general induction b.
-- unfold decidable. left. simpl. intuition.
--  destruct IHb, a.
-   + unfold decidable. left. simpl.  destruct b; eauto.
-   + unfold decidable. right. intuition.
-   + unfold decidable. right. intuition.  simpl in H0.  destruct b; eauto.
-   + unfold decidable. right. intuition.
-Qed.
-
-Fixpoint bvTrue b :bool :=
-match b with
-| nil => false
-| O::nil => false
-| a::b => match a with
-              | I => true
-              | O => bvTrue b
-          end
-end.
-
-Lemma true_decidable:
-forall b, decidable (bvTrue b).
-
-Proof.
-intros. unfold decidable. general induction b.
--  right.  intuition.
--  destruct IHb, a.
-   + left. simpl. destruct b; eauto.
-   + left. simpl. intuition.
-   + right. unfold bvTrue.  destruct b; intuition. 
-   + left.  unfold bvTrue. intuition.
-Qed.
-
-Definition bitEq b1 b2 :=
-match b1, b2 with 
-| I, I => true
-| O, O => true
-| _, _ => false
-end.
-
-Fixpoint bvEq b1 b2 :=
-match b1, b2 with
-| nil, nil => true
-| nil, _ => false
-| _, nil => false
-| a1::b1', a2::b2' => match bitEq a1 a2 with
-                        |true => bvEq b1' b2'
-                        |false => false
-                      end
-
-end.
-
-Lemma equality_decidable:
-forall b1 b2, decidable (bvEq b1 b2).
-
-Proof.
-intros.  general induction  b1. 
-- destruct b2. 
-  + left.  intuition.
-  +  right. intuition.
-- destruct b2. 
-  + right.  intuition.
-  +   simpl.  destruct (bitEq a b).
-   * destruct (IHb1 b2); eauto.
-   * right; intuition.
-Qed.
-
-(*
 Lemma is_not_zero b:
 b <> nil /\ b <> O::nil -> isZero b = false.
 
@@ -504,23 +569,7 @@ intros. destruct b.
     * reflexivity. 
   + simpl. destruct b; reflexivity.
 Qed.
-*)
 
-Fixpoint bvMult' n (b:bitvec) akk :=
-match n with
-|0 => O::nil
-|1 => akk
-|S n => match b with
-            | nil => O::nil 
-            | O::nil => O::nil
-            | _ => bvMult' n b (bvAdd b akk O)
-         end
-end.
-
-Definition bvMult b1 b2 :=
-bvMult' (bitvecToNat b1) b2 b2.
-
-(*
 Lemma agree_on_mult b1 b2 n1 n2:
 bitvecToNat b1 = n1
 ->bitvecToNat b2 = n2
@@ -548,56 +597,7 @@ intros. general induction b1.
        { unfold bvMult. rewrite (is_not_zero H). 
          assert (H1: (O::b::b2) <> nil /\ (O::b::b2) <> O::nil) by (split; hnf; intros; discriminate).
          rewrite (is_not_zero H1). 
-*)
 
-Fixpoint nbvSub b c :=
-match b with 
-  |a::b' => match  c,a with
-              |O,O => O::b'
-              |O,I => I::b'
-              |I,O => I:: nbvSub b' I
-              |I,I => O::b'
-            end
-  |nil => nil
-end.
-
-
-Fixpoint bvSub (b1:bitvec) (b2:bitvec) (c:bit):=
-match bvGeq b1 b2 with
-  |true =>  match b1 with
-                |a::b1' => match b2 with
-                               | b::b2' => match c,a,b with 
-                                               |O,O,O => O:: bvSub b1' b2' O
-                                               |I,O,O => I:: bvSub b1' b2' I
-                                               |O,I,O => I:: bvSub b1' b2' O
-                                               |I,I,O => O:: bvSub b1' b2' O
-                                               |O,O,I => I:: bvSub b1' b2' I
-                                               |I,O,I => O:: bvSub b1' b2' I
-                                               |O,I,I => O:: bvSub b1' b2' O
-                                               |I,I,I => I:: bvSub b1' b2' I
-                                            end
-                               | nil => nbvSub b1 c                                    
-                             end
-                |nil    => nil
-            end
-  |false => O::nil
-end.
-
-(** a / b = None, b = 0, Some ( S (a -b)/b) **)
-Fixpoint bvDiv' (a:nat) (b:bitvec) (akk:bitvec) :=
-match a with
-| 0 => Some akk
-| S n' => match b with
-           | nil => None
-           | O::nil => None
-           | _ => bvDiv' (min n' (a - (bitvecToNat b))) b (incr akk)
-          end
-end.
-
-Definition bvDiv (b1:bitvec) (b2:bitvec) :option bitvec:=
-bvDiv' (bitvecToNat b1) b2 (O::nil).
-
-(*
 Lemma nbvSub_decrease b c :
 bitvecToNat (nbvSub b c) = bitvecToNat b - bitToNat c.
 
@@ -656,60 +656,6 @@ intros. general induction b1.
   
   + admit.
 *)
-
-(**
-evalSexp evaluates an SMT expression given an environment with the variable bindings already 
-introduced and the expression to evaluate. It's returntype is a bitvector option as it is
-possible that
-a) a variable occurs free
-b) a variable has not been defined
-c) evaluation can become stuck
-**)
-
-Fixpoint evalSexp (E: senv)(s:sexp):option bitvec :=
-match s with
-|plus a b 
-  => let v1 := evalSexp E a in
-     let v2 := evalSexp E b in
-     match v1,v2 with
-         | Some v1, Some v2 => Some (bvAdd v1 v2 O)
-         | _, _ => None
-     end
-|sub a b
-  => let v1 := evalSexp E a in
-     let v2 := evalSexp E b in
-     match v1,v2 with
-         | Some v1, Some v2 => Some (bvSub v1 v2 O)
-         | _,_=> None
-     end
-|mult a b
-  => let v1 := evalSexp E a in
-     let v2 := evalSexp E b in
-     match v1,v2 with
-         | Some v1, Some v2 => Some (bvMult v1 v2)
-         | _,_ => None
-     end
-|div a b  
-  => let v1 := evalSexp E a in
-     let v2 := evalSexp E b in
-     match v1, v2 with
-         | Some v1, Some v2 => bvDiv v1 v2
-         | _,_ => None
-     end
-| sneg v  (* TODO!*)
- => let v' := evalSexp E v in
-    match v' with
-      | Some v => Some v 
-      | None => None
-    end
-| const c
-  => Some c
-| svar v
-  => match (E v) with
-         | Some b => Some b
-         | None => None
-     end
-end.
 
 (*
 *** Local Variables: ***
