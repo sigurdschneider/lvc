ecbfc27224045a4b6eaf5467a549ef45cddf28eb
Heiko Becker <heikobecker92@gmail.com>
2014-10-10 19:43:58 +0200

    Add draft of OrderedType bitvec

---
+++ TransVal/orderedBitvec.v
@@ -0,0 +1,222 @@
+Require Import Util List OrderedType bitvec.
+
+(** Inductive lt predicate for bits **)
+Inductive ltBit :bit-> bit -> Prop :=
+|ltBitO :  ltBit O I.
+
+(** Equality predicate for bits *)
+Inductive eqBit : bit->bit -> Prop :=
+|eqBitO :  eqBit O O
+|eqBitI :  eqBit I I.
+
+Definition bitCmp (b1:bit) (b2:bit) :=
+match b1,b2 with
+|O, O => Eq
+|O,I => Lt
+| I, O => Gt
+| I, I => Eq
+end.
+
+Lemma bitEq_reflexive :
+Reflexive eqBit.
+
+Proof.
+unfold Reflexive. intros. destruct x; econstructor.
+Qed.
+
+Lemma bitEq_symm:
+Symmetric eqBit.
+
+Proof.
+unfold Symmetric. intros. general induction H; econstructor.
+Qed.
+
+Lemma bitEq_trans:
+Transitive eqBit.
+
+Proof.
+unfold Transitive. intros. general induction H.
+- assumption.
+- assumption.
+Qed.
+
+Instance equiv_eqbit: Equivalence eqBit.
+econstructor.
+- exact bitEq_reflexive.
+- exact bitEq_symm.
+- exact bitEq_trans.
+Defined.
+
+Lemma ltBit_trans:
+Transitive ltBit.
+
+Proof.
+unfold Transitive. intros. general induction H.
+- general induction H0.
+Qed.
+
+Lemma ltBit_irrefl:
+Irreflexive ltBit.
+
+Proof.
+hnf; intros. unfold RelationClasses.complement. induction x.
+- intros. inversion H.
+- intros. inversion H.
+Qed.
+
+Instance  rewrite_eqBit: forall x, Proper (eqBit ==> flip impl) (ltBit x).
+unfold Proper, respectful. intros.  unfold flip.  unfold impl.  intros.  general induction H0.
+- general induction H. econstructor.
+Defined.
+
+Instance lt_eq_bit_strict: OrderedType.StrictOrder ltBit eqBit.
+econstructor.
+- exact ltBit_trans.
+- intros. intro. apply (ltBit_irrefl x). rewrite <- H0 in H. assumption.
+Defined.
+
+Instance OrderedType_bit : OrderedType bit :=
+{ _eq := eqBit;
+  _lt := ltBit;
+  _cmp := bitCmp}.
+intros. general induction x; destruct y; simpl; try now (econstructor; eauto using ltBit).
+Qed.
+
+(** Inductive lt predicate for bitvectors **)
+Inductive ltBitvec : bitvec -> bitvec -> Prop :=
+|ltBitvecNil a b: ltBitvec nil (a::b)
+|ltBitvecCons a b c d : _lt a c -> ltBitvec b d -> ltBitvec (a::b) (c::d).
+(*|ltBitvecEq a b c d: _eq a c -> ltBitvec b d -> ltBitvec (a::b) (c::d) *)
+
+(** Equality predicate for bitvectors **)
+Inductive eqBitvec:  bitvec -> bitvec -> Prop :=
+|eqNil: eqBitvec nil nil
+|eqCons a b c d: _eq a c -> eqBitvec b d -> eqBitvec (a::b) (c::d).
+
+Fixpoint bvCmp  (a:bitvec) ( b:bitvec) :=
+match a,b with
+| nil, nil => Eq
+| nil, _ => Lt
+| _, nil => Gt
+|a1::a' ,b1::b' => match( _cmp a1 b1 ) with
+                         | Eq => bvCmp a' b'
+                         | z => z
+                   end
+end.
+(*                       | Lt, Lt => Lt
+                       | Lt, Gt => Lt (* TODO *)
+                       | Lt, Eq => Lt
+                       |Eq, Lt => Lt
+                       |Eq, Eq => Eq
+                       |Eq, Gt => Gt
+                       |Gt, Lt => Gt (* TODO *)
+                       |Gt, Eq => Gt
+                       |Gt, Gt => Gt
+                                    end
+end.*)
+
+Lemma bitvecEq_reflexive :
+Reflexive eqBitvec.
+
+Proof.
+unfold Reflexive. intros. general induction x.
+- econstructor.
+-  econstructor.
+   * eauto.
+   * assumption.
+Qed. 
+
+Lemma bitvecEq_symm:
+Symmetric eqBitvec.
+
+Proof.
+unfold Symmetric. intros. general induction H.
+- econstructor.
+- econstructor.  
+  * eauto.
+  * assumption.
+Qed.
+
+Lemma bitvecEq_trans:
+Transitive eqBitvec.
+
+Proof.
+unfold Transitive. intros. general induction H.
+- assumption.
+-  general induction H1.
+   + econstructor. 
+     *  apply transitivity with c0; eauto.
+     * eapply (IHeqBitvec0). assumption.
+Qed.
+
+Instance equiv_eqbitvec : Equivalence eqBitvec.
+econstructor.
+- exact bitvecEq_reflexive.
+- exact bitvecEq_symm.
+- exact bitvecEq_trans.
+Defined.  
+
+Lemma ltBitvec_trans:
+Transitive ltBitvec.
+
+Proof.
+unfold Transitive. intros. general induction H.
+- general induction H0. 
+  + econstructor.
+-  general induction H1.
+   +  econstructor. 
+      * eapply transitivity with c0; eauto. 
+      * apply (IHltBitvec0 d). assumption.
+Qed.
+
+Lemma ltBitvec_irrefl:
+Irreflexive ltBitvec.
+
+Proof.
+hnf; intros. unfold RelationClasses.complement.  induction x.
+- intros. inversion H.
+- intros. inversion H. eapply IHx. assumption.
+Qed.
+
+ Instance rewrite_equal_bitvec: forall x, Proper (equiv ==> impl) (ltBitvec x) .
+unfold Proper, respectful. intros.  unfold impl. intros. general induction H0.
+- general induction H. econstructor.
+- general induction H1. econstructor.
+  +  rewrite <-  H. assumption.
+  + apply (IHltBitvec d).  hnf. assumption.
+Defined.
+
+Instance lt_eq_strict : OrderedType.StrictOrder ltBitvec eqBitvec.
+econstructor.
+- exact ltBitvec_trans.
+- intros. intro. apply (ltBitvec_irrefl x).   rewrite <-  H0 in H.  assumption. 
+Defined.
+
+Instance OrderedType_bitvec : OrderedType bitvec := 
+  { _eq := eqBitvec;
+     _lt := ltBitvec;
+      _cmp := bvCmp}.
+intros. general induction x; destruct y; simpl; try now (econstructor; eauto using ltBitvec).
+pose proof (_compare_spec a b); destruct (IHx y);
+inv H;  try now (econstructor; eauto using ltBitvec).
+- econstructor; econstructor.
+
+- assert (X: ltBitvec (a::x) (b::y)) by (econstructor; eauto; admit); now (econstructor; eauto using ltBitvec). 
+- assert (X: ltBitvec (b::y) (a::x)) by (econstructor; eauto; admit); now (econstructor; eauto using ltBitvec). 
+- admit.
+- admit.
+- admit.
+- admit.
+- admit.
+- admit.
+
+- assert (X: ltBitvec nil (b::y)) by (exact (ltBitvecNil b y)). exact (compare_spec_lt eqBitvec ltBitvec nil (b::y) X).
+- assert (X: ltBitvec nil (a::x)) by (exact (ltBitvecNil a x)). exact (compare_spec_gt eqBitvec ltBitvec (a::x) nil X).
+-  destruct (IHx y).
+   + 
+
+(*
+*** Local Variables: ***
+*** coq-load-path: (("../" "Lvc")) ***
+*** End: ***
+*)1
