463ea70ce9a56be267d6af379e10fca7fbe19c27
Heiko Becker <heikobecker92@gmail.com>
2014-11-05 21:13:31 +0100

    Fix compile issue with terminates from Relations

--- TransVal/Terminates.v
+++ TransVal/Terminates.v
@@ -4,7 +4,6 @@ Set Implicit Arguments.
 
 Inductive Terminates :F.state -> F.state -> Prop :=
 |TerminatesReturn L E e v:
-(*   translateExp e = ⎣b⎦  *)
    exp_eval E e = ⎣v⎦
    -> Terminates (L, E, stmtReturn e)   (L  , E , stmtReturn e)
 |TerminatesStep sigma sigma' sigma'' a:
