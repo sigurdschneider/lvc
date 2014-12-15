f34ba3c6bf8f1577c305050f2d0401921034c534
Heiko Becker <heikobecker92@gmail.com>
2014-12-15 17:13:37 +0100

    Start clean up

--- TransVal/nofun.v
+++ TransVal/nofun.v
@@ -1,4 +1,4 @@
-Require Import IL.
+Require Import IL Terminates Crash.
 
 Inductive noFun : stmt->Prop :=
 |noFunLet x e s :
@@ -12,6 +12,63 @@ Inductive noFun : stmt->Prop :=
  noFun (stmtGoto l Y)
 |noFunExp e :
  noFun (stmtReturn e).
+
+Lemma noFun_impl_term_crash :
+forall E s,
+noFun s
+->  exists E' s',
+      forall L,
+        Terminates (L, E, s)(L, E', s') \/ Crash (L, E, s) (L, E', s').
+
+Proof.
+intros.
+general induction H.
+-  case_eq (exp_eval E e); intros.
+   + destruct (IHnoFun (E[x<- Some v])) as [E' [s' termcrash]].
+     exists E'; exists s'; intros.
+     destruct (termcrash L) as [ term | crash].
+     * left. econstructor; eauto.
+              { econstructor; eauto. }
+              { intros. hnf; isabsurd. }
+     * right. econstructor; eauto; try  econstructor; eauto; intros; isabsurd.
+   + exists E; exists (stmtExp x e s); intros.
+       right. econstructor; eauto; intros; try hnf; try isabsurd.
+       intros. unfold reducible2 in H1.  destruct H1. destruct H1.
+       isabsurd.
+- case_eq (exp_eval E e); intros.
+  + case_eq (val2bool v); intros.
+    * destruct (IHnoFun1 E) as [E' [s' termcrash]].
+      exists E'; exists s'; intros.
+      destruct (termcrash L) as [term | crash].
+      { left. econstructor; eauto.
+       - econstructor; eauto.
+       - hnf; intros; isabsurd. }
+      { right. econstructor; eauto. econstructor; eauto.
+      intros; isabsurd. }
+    * destruct (IHnoFun2 E) as [E' [s' termcrash]];
+      exists E'; exists s'; intros.
+      destruct (termcrash L) as [ term | crash ].
+      { left. econstructor; eauto.
+       - econstructor; eauto.
+       - hnf; intros; isabsurd. }
+      { right. econstructor; eauto. econstructor; eauto.
+      intros; isabsurd. }
+  +  exists E; exists (stmtIf e s t).
+            right. econstructor; eauto; intros; try hnf; try isabsurd.
+            intros. unfold reducible2 in H2. destruct H2. destruct H2.
+            isabsurd.
+- case_eq (omap (exp_eval E) Y); intros.
+  + exists E; exists (stmtGoto l Y); left; econstructor; eauto.
+  + exists E; exists (stmtGoto l Y); right.
+           econstructor; eauto; intros; try hnf; try isabsurd.
+- case_eq (exp_eval E e).
+  + exists E; exists (stmtReturn e).
+           left; econstructor; eauto.
+  + exists E; exists (stmtReturn e). right.
+           econstructor; eauto; intros; try hnf; try isabsurd.
+           intros. unfold reducible2 in H0. destruct H0.
+           destruct H0. inversion H0.
+Qed.
 
 (*
 *** Local Variables: ***
--- TransVal/transval.v
+++ TransVal/transval.v
@@ -10,62 +10,6 @@ smtAnd (translateStmt s source) (translateStmt t target).
 Definition combineEnv D (E1:onv val) E2 :=
 fun x => if [x ∈ D] then E1 x else E2 x.
 
-Lemma noFun_impl_term_crash :
-forall E s,
-noFun s
-->  exists E' s', forall L, Terminates (L, E, s)(L, E', s')
-                            \/ Crash (L, E, s) (L, E', s').
-
-Proof.
-intros.
-general induction H.
--  case_eq (exp_eval E e); intros.
-   + destruct (IHnoFun (E[x<- Some v])) as [E' [s' termcrash]].
-     exists E'; exists s'; intros.
-     destruct (termcrash L) as [ term | crash].
-     * left. econstructor; eauto.
-              { econstructor; eauto. }
-              { intros. hnf; isabsurd. }
-     * right. econstructor; eauto; try  econstructor; eauto; intros; isabsurd.
-   + exists E; exists (stmtExp x e s).
-       right. econstructor; eauto; intros; try hnf; try isabsurd.
-       intros. unfold reducible2 in H1.  destruct H1. destruct H1.
-       isabsurd.
-- case_eq (exp_eval E e); intros.
-  + case_eq (val2bool v); intros.
-    * destruct (IHnoFun1 E) as [E' [s' termcrash]].
-      exists E'; exists s'; intros.
-      destruct (termcrash L) as [term | crash].
-      { left. econstructor; eauto.
-       - econstructor; eauto.
-       - hnf; intros; isabsurd. }
-      { right. econstructor; eauto. econstructor; eauto.
-      intros; isabsurd. }
-    * destruct (IHnoFun2 E) as [E' [s' termcrash]];
-      exists E'; exists s'; intros.
-      destruct (termcrash L) as [ term | crash ].
-      { left. econstructor; eauto.
-       - econstructor; eauto.
-       - hnf; intros; isabsurd. }
-      { right. econstructor; eauto. econstructor; eauto.
-      intros; isabsurd. }
-  +  exists E; exists (stmtIf e s t).
-            right. econstructor; eauto; intros; try hnf; try isabsurd.
-            intros. unfold reducible2 in H2. destruct H2. destruct H2.
-            isabsurd.
-- case_eq (omap (exp_eval E) Y); intros.
-  + exists E; exists (stmtGoto l Y); left; econstructor; eauto.
-  + exists E; exists (stmtGoto l Y); right.
-           econstructor; eauto; intros; try hnf; try isabsurd.
-- case_eq (exp_eval E e).
-  + exists E; exists (stmtReturn e).
-           left; econstructor; eauto.
-  + exists E; exists (stmtReturn e). right.
-           econstructor; eauto; intros; try hnf; try isabsurd.
-           intros. unfold reducible2 in H0. destruct H0.
-           destruct H0. inversion H0.
-Qed.
-
 Lemma freeVars_undef :
 forall a e s,
 undef e = Some s
