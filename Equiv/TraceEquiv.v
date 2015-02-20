d186f9fe0d503753257f62b12c34091a8a8fa2ac
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2015-02-20 12:51:29 +0100

    Remove unused definition.

--- Equiv/TraceEquiv.v
+++ Equiv/TraceEquiv.v
@@ -9,23 +9,6 @@ Inductive extevent :=
   | EEvt (evt:event)
   | EvtTerminate (res:option val).
 
-
-Inductive produces {S} `{StateType S} : S -> list extevent -> Prop :=
-  | producesSilent (σ:S) (σ':S) L :
-      step σ EvtTau σ'
-      -> produces σ' L
-      -> produces σ  L
-  | producesExtern (σ:S) (σ':S) evt L :
-      activated σ
-      -> step σ evt σ'
-      -> produces σ' L
-      -> produces σ (EEvt evt::L)
-  | producesTerm (σ:S) (σ':S) r
-    : result σ' = r
-      -> star2 step σ nil σ'
-      -> normal2 step σ'
-      -> produces σ (EvtTerminate r::nil).
-
 Inductive produces_prefix {S} `{StateType S} : S -> list extevent -> Prop :=
   | producesPrefixSilent (σ:S) (σ':S) L :
       step σ EvtTau σ'
@@ -43,15 +26,6 @@ Inductive produces_prefix {S} `{StateType S} : S -> list extevent -> Prop :=
       -> produces_prefix σ (EvtTerminate r::nil)
   | producesPrefixPrefix (σ:S)
     : produces_prefix σ nil.
-
-Lemma produces_star2_silent {S} `{StateType S} (σ σ':S) L
- : star2 step σ nil σ' ->
-   produces σ' L -> produces σ L.
-Proof.
-  intros. general induction H0; eauto.
-  - destruct yl, y; simpl in *; try congruence.
-    econstructor 1; eauto.
-Qed.
 
 Lemma produces_prefix_star2_silent {S} `{StateType S} (σ σ':S) L
  : star2 step σ nil σ' ->
@@ -101,32 +75,33 @@ Proof.
   dcr; congruence.
 Qed.
 
-Lemma bisim_produces {S} `{StateType S} {S'} `{StateType S'} (sigma:S) (σ':S')
-  : bisim sigma σ' -> forall L, produces sigma L -> produces σ' L.
+Lemma bisim_prefix {S} `{StateType S} {S'} `{StateType S'} (sigma:S) (σ':S')
+: bisim sigma σ' -> forall L, produces_prefix sigma L -> produces_prefix σ' L.
 Proof.
   intros. general induction H2.
   - eapply bisim_bisim' in H3.
-    eapply IHproduces; eauto.
+    eapply IHproduces_prefix; eauto.
     eapply bisim'_bisim. eapply bisim'_reduction_closed_1; eauto.
     eapply (S_star2 _ _ H0). eapply star2_refl.
   - eapply bisim_bisim' in H4.
     eapply bisim'_activated in H4; eauto.
     destruct H4 as [? [? [? []]]].
     destruct evt.
-    + eapply produces_star2_silent; eauto.
+    + eapply produces_prefix_star2_silent; eauto.
       edestruct H6; eauto. destruct H8.
       econstructor 2. eauto. eapply H8.
-      eapply IHproduces; eauto.
+      eapply IHproduces_prefix; eauto.
       eapply bisim'_bisim. eapply H9.
     + exfalso; eapply (no_activated_tau_step _ H0 H1); eauto.
   - eapply bisim_bisim' in H4. eapply bisim'_terminate in H4; eauto.
     destruct H4 as [? [? []]]. econstructor 3; [ | eauto | eauto]. congruence.
+  - econstructor 4.
 Qed.
 
-Lemma bisim_trace_eq {S} `{StateType S} {S'} `{StateType S'} (sigma:S) (σ':S')
-  : bisim sigma σ' -> forall L, produces sigma L <-> produces σ' L.
+Lemma bisim_prefix_eq {S} `{StateType S} {S'} `{StateType S'} (sigma:S) (σ':S')
+  : bisim sigma σ' -> forall L, produces_prefix sigma L <-> produces_prefix σ' L.
 Proof.
-  split; eapply bisim_produces; eauto.
+  split; eapply bisim_prefix; eauto.
   eapply bisim_sym; eauto.
 Qed.
 
