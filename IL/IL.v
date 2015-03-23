d68cd3ddc5c46ef94f7b3384b4b394ee441e1a65
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2015-03-23 16:37:00 +0100

    Add mutual recursion to definition of IL.

--- IL/IL.v
+++ IL/IL.v
@@ -1,5 +1,5 @@
 Require Import List.
-Require Export Util Relations Get Drop Var Val Exp Env Map CSet AutoIndTac MoreList OptionMap Events.
+Require Export Util Relations Get Drop Var Val Exp Env Map CSet AutoIndTac MoreList OptionMap Events Size.
 Require Import SetOperations.
 
 Set Implicit Arguments.
@@ -17,11 +17,13 @@ Notation "'params'" := (list var) (at level 0).
 Inductive stmt : Type :=
 | stmtLet    (x : var) (e: exp) (s : stmt) : stmt
 | stmtIf     (e : exp) (s : stmt) (t : stmt) : stmt
-| stmtApp   (l : lab) (Y:args) : stmt
+| stmtApp    (l : lab) (Y:args) : stmt
 | stmtReturn (e : exp) : stmt
 | stmtExtern (x : var) (f:external) (Y:args) (s:stmt)
 (* block f Z : rt = s in b *)
-| stmtFun    (Z:params) (s : stmt) (t : stmt) : stmt.
+| stmtFun    (s:list (params * stmt)) (t : stmt) : stmt.
+
+Instance Stmt_size : Size stmt. gen_Size. Defined.
 
 Fixpoint freeVars (s:stmt) : set var :=
   match s with
@@ -30,7 +32,8 @@ Fixpoint freeVars (s:stmt) : set var :=
     | stmtApp l Y => list_union (List.map Exp.freeVars Y)
     | stmtReturn e => Exp.freeVars e
     | stmtExtern x f Y s => (freeVars s \ {{x}}) ∪ list_union (List.map Exp.freeVars Y)
-    | stmtFun Z s1 s2 => (freeVars s1 \ of_list Z) ∪ freeVars s2
+    | stmtFun s t =>
+      list_union (List.map (fun f => (freeVars (snd f) \ of_list (fst f))) s) ∪ freeVars t
   end.
 
 Fixpoint definedVars (s:stmt) : set var :=
@@ -40,7 +43,8 @@ Fixpoint definedVars (s:stmt) : set var :=
     | stmtApp l Y => ∅
     | stmtReturn e => ∅
     | stmtExtern x f Y s => {x; definedVars s}
-    | stmtFun Z s1 s2 => definedVars s1 ∪ definedVars s2 ∪ of_list Z
+    | stmtFun s t =>
+      list_union (List.map (fun f => (definedVars (snd f) ∪ of_list (fst f))) s) ∪ definedVars t
   end.
 
 Fixpoint occurVars (s:stmt) : set var :=
@@ -50,35 +54,91 @@ Fixpoint occurVars (s:stmt) : set var :=
     | stmtApp l Y => list_union (List.map Exp.freeVars Y)
     | stmtReturn e => Exp.freeVars e
     | stmtExtern x f Y s => {x; occurVars s} ∪ list_union (List.map Exp.freeVars Y)
-    | stmtFun Z s1 s2 => occurVars s1 ∪ occurVars s2 ∪ of_list Z
+    | stmtFun s t =>
+      list_union (List.map (fun f => (occurVars (snd f) ∪ of_list (fst f))) s) ∪ occurVars t
   end.
+
+Lemma list_union_f_incl X `{OrderedType X} Y (f g:Y->set X) s
+: (forall n y, get s n y -> f y ⊆ g y)
+  -> list_union (List.map f s) ⊆ list_union (List.map g s).
+Proof.
+  intros. general induction s; simpl; eauto.
+  unfold list_union; simpl.
+  repeat rewrite list_union_start_swap.
+  rewrite IHs, H0; eauto using get; reflexivity.
+Qed.
+
+Lemma list_union_f_eq X `{OrderedType X} Y (f g:Y->set X) s
+: (forall n y, get s n y -> f y [=] g y)
+  -> list_union (List.map f s) [=] list_union (List.map g s).
+Proof.
+  intros. general induction s; simpl; eauto.
+  unfold list_union; simpl.
+  repeat rewrite list_union_start_swap.
+  rewrite IHs, H0; eauto using get; reflexivity.
+Qed.
+
+Hint Extern 10 (size _ < size _) => somega.
 
 Lemma freeVars_occurVars s
 : freeVars s ⊆ occurVars s.
 Proof.
-  general induction s; simpl; cset_tac; intuition.
+  sind s; destruct s; simpl in * |- *; eauto.
+  - rewrite IH; cset_tac; intuition.
+  - repeat rewrite IH; eauto.
+  - rewrite IH; eauto. cset_tac; intuition.
+  - rewrite IH, list_union_f_incl; eauto. reflexivity.
+    intros. destruct y; simpl. rewrite IH; cset_tac; intuition; eauto.
+Qed.
+
+Hint Resolve incl_empty : cset.
+
+Lemma list_union_f_union X `{OrderedType X} Y (f g:Y->set X) s
+: list_union (List.map f s) ∪ list_union (List.map g s) [=]
+             list_union (List.map (fun x => f x ∪ g x) s).
+Proof.
+  intros. general induction s; simpl; eauto.
+  - unfold list_union; simpl. cset_tac; intuition.
+  - unfold list_union; simpl.
+  repeat rewrite list_union_start_swap.
+  rewrite <- IHs; eauto using get. cset_tac; intuition.
+Qed.
+
+
+
+Lemma occurVars_freeVars_definedVars s
+: occurVars s [=] freeVars s ∪ definedVars s.
+Proof.
+  sind s; destruct s; simpl in * |- *; eauto with cset.
+  - rewrite IH; eauto. clear_all; cset_tac; intuition; eauto.
+    decide (x === a); intuition; eauto.
+  - repeat rewrite IH; eauto. clear_all; cset_tac; intuition; eauto.
+  - cset_tac; intuition.
+  - cset_tac; intuition.
+  - rewrite IH; eauto. clear_all; cset_tac; intuition; eauto.
+    decide (x === a); intuition; eauto.
+  - rewrite IH; eauto.
+    repeat setoid_rewrite union_assoc at 1.
+    setoid_rewrite union_comm at 5.
+    repeat setoid_rewrite <- union_assoc.
+    erewrite list_union_f_union.
+    rewrite list_union_f_eq. cset_tac; intuition; eauto.
+    simpl. intros. rewrite IH; eauto.
+    clear_all; cset_tac; intuition; eauto. simpl in *.
+    decide (a ∈ of_list a0); intuition; eauto.
 Qed.
 
 Lemma definedVars_occurVars s
 : definedVars s ⊆ occurVars s.
 Proof.
-  general induction s; simpl; try now (cset_tac; intuition).
+  sind s; destruct s; simpl in * |- *; eauto with cset.
+  - rewrite IH; cset_tac; intuition.
+  - repeat rewrite IH; eauto.
+  - rewrite IH; eauto.
+  - rewrite IH, list_union_f_incl; eauto. reflexivity.
+    intros. destruct y; simpl. rewrite IH; cset_tac; intuition; eauto.
 Qed.
 
-Lemma occurVars_freeVars_definedVars s
-: occurVars s [=] freeVars s ∪ definedVars s.
-Proof.
-  general induction s; simpl.
-  - rewrite IHs. clear_all; cset_tac; intuition; eauto.
-    decide (x === a); intuition; eauto.
-  - rewrite IHs1, IHs2. clear_all; cset_tac; intuition; eauto.
-  - cset_tac; intuition.
-  - cset_tac; intuition.
-  - rewrite IHs. clear_all; cset_tac; intuition; eauto.
-    decide (x === a); intuition; eauto.
-  - rewrite IHs1, IHs2. clear_all; cset_tac; intuition; eauto.
-    decide (a ∈ of_list Z); intuition; eauto.
-Qed.
 
 (** ** Semantics *)
 
@@ -89,11 +149,18 @@ Module F.
     blockI {
       block_E : onv val;
       block_Z : params;
-      block_s : stmt
+      block_s : stmt;
+      block_n : nat
     }.
 
   Definition labenv := list block.
   Definition state : Type := (labenv * onv val * stmt)%type.
+
+  Definition mkBlock E n f :=
+    blockI E (fst f) (snd f) n.
+
+  Definition mkBlocks E F :=
+    mapi (mkBlock E) F.
 
   Inductive step : state -> event -> state -> Prop :=
   | stepExp L E x e b v
@@ -119,11 +186,11 @@ Module F.
     (updOk:(block_E blk) [block_Z blk <-- List.map Some vl] = E')
     : step  (L, E, stmtApp l Y)
             EvtTau
-            (drop (counted l) L, E', block_s blk)
+            (drop (counted l - block_n blk) L, E', block_s blk)
 
   | stepLet L E
-    s Z (t:stmt)
-    : step (L, E, stmtFun Z s t) EvtTau (blockI E Z s::L, E, t)
+    s (t:stmt)
+    : step (L, E, stmtFun s t) EvtTau ((mkBlocks E s ++ L)%list, E, t)
 
   | stepExtern L E x f Y s vl v
     (def:omap (exp_eval E) Y = Some vl)
@@ -174,11 +241,14 @@ Module I.
   Inductive block : Type :=
     blockI {
       block_Z : params;
-      block_s : stmt
+      block_s : stmt;
+      block_n : nat
     }.
 
   Definition labenv := list block.
   Definition state : Type := (labenv * onv val * stmt)%type.
+  Definition mkBlock n f := blockI (fst f) (snd f) n.
+  Definition mkBlocks F := mapi mkBlock F.
 
   Inductive step : state -> event -> state -> Prop :=
   | stepExp L E x e b v
@@ -204,12 +274,12 @@ Module I.
     (updOk:E[block_Z blk  <-- List.map Some vl] = E')
     : step  (L, E, stmtApp l Y)
             EvtTau
-            (drop (counted l) L, E', block_s blk)
+            (drop (counted l - block_n blk) L, E', block_s blk)
 
 
   | stepLet L E
-    s Z (b:stmt)
-    : step (L, E, stmtFun Z s b) EvtTau (blockI Z s::L, E, b)
+    s (t:stmt)
+    : step (L, E, stmtFun s t) EvtTau ((mkBlocks s ++ L)%list, E, t)
 
   | stepExtern L E x f Y s vl v
     (def:omap (exp_eval E) Y = Some vl)
@@ -254,13 +324,6 @@ Module I.
   Qed.
 
 End I.
-
-Fixpoint labenvRmE (L:F.labenv) : I.labenv
-  :=
-  match L with
-    | nil => nil
-    | F.blockI E Z s::L => I.blockI Z s :: labenvRmE L
-  end.
 
 (*
 *** Local Variables: ***
