ceb4ed77f5ed6d6b40a75ab4fe1a03a276e1f70d
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2015-03-23 16:55:52 +0100

    Update rename for mutual recursion.

--- IL/Rename.v
+++ IL/Rename.v
@@ -11,7 +11,8 @@ Fixpoint rename (ϱ:env var) (s:stmt) : stmt :=
     | stmtApp l Y => stmtApp l (List.map (rename_exp ϱ) Y)
     | stmtReturn e => stmtReturn (rename_exp ϱ e)
     | stmtExtern x f e s => stmtExtern (ϱ x) f (List.map (rename_exp ϱ) e) (rename ϱ s)
-    | stmtFun Z s t => stmtFun (lookup_list ϱ Z) (rename ϱ s) (rename ϱ t)
+    | stmtFun s t =>
+      stmtFun (List.map (fun Zs => (lookup_list ϱ (fst Zs), (rename ϱ (snd Zs)))) s) (rename ϱ t)
   end.
 
 (** Renaming respects function equivalence *)
@@ -20,15 +21,24 @@ Global Instance rename_morphism
   : Proper (@feq _ _ _eq ==> eq ==> eq) rename.
 Proof.
   unfold Proper, respectful; intros; subst.
-  general induction y0; simpl; f_equal; eauto; try (now rewrite H; eauto);
+  sind y0; destruct y0; simpl; f_equal; eauto; try (now rewrite H; eauto);
   eauto using rename_exp_ext, map_ext_get_eq; eauto.
+  eapply map_ext_get_eq; intros. f_equal; eauto. rewrite H; eauto.
 Qed.
 
 Lemma rename_agree ϱ ϱ' s
 : agree_on eq (occurVars s) ϱ ϱ'
   -> rename ϱ s = rename ϱ' s.
 Proof with eauto 50 using rename_exp_agree, map_ext_get_eq with cset.
-  intros. general induction s; simpl in *; f_equal...
+  intros.
+  sind s; destruct s; simpl in *; f_equal...
+  eapply map_ext_get_eq; intros. f_equal; eauto.
+  - erewrite lookup_list_agree; eauto.
+    eapply agree_on_incl; eauto.
+    rewrite <- get_list_union_map; eauto with cset. eauto with cset.
+  - eapply IH; eauto.
+    eapply agree_on_incl; eauto.
+    rewrite <- get_list_union_map; eauto with cset. eauto with cset.
 Qed.
 
 
