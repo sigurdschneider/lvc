9f33a59c2a9e58cc70017b4dda94353e1d2a016e
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2015-05-06 12:47:34 +0200

    Fix liveness validators for mutual.

--- LivenessValidators.v
+++ LivenessValidators.v
@@ -15,14 +15,15 @@ Defined.
 Definition live_sound_dec i Lv s slv (an:annotation s slv)
       : Computable (live_sound i Lv s slv).
 Proof.
-  general induction s; destruct slv; try isabsurd.
-  + edestruct IHs; eauto; try inv an; eauto;
+  revert i Lv slv an.
+  sind s; intros; destruct s; destruct slv; try isabsurd.
+  + edestruct (IH s); eauto; try inv an; eauto;
     decide (getAnn slv\{{x}} ⊆ a);
     decide (live_exp_sound e a);
     decide (x ∈ getAnn slv);
     try dec_solve.
-  + edestruct IHs1; try inv an; eauto;
-    edestruct IHs2; try inv an; eauto;
+  + edestruct (IH s1); try inv an; eauto;
+    edestruct (IH s2); try inv an; eauto;
     decide (live_exp_sound e a);
     decide (getAnn slv1 ⊆ a);
     decide (getAnn slv2 ⊆ a);
@@ -36,21 +37,26 @@ Proof.
       get_functional; subst. eauto.
     - left; econstructor; eauto. destruct if; eauto. intuition.
   + decide(live_exp_sound e a); try dec_solve.
-  + edestruct IHs; eauto; try inv an; eauto;
+  + edestruct (IH s); eauto; try inv an; eauto;
     decide (getAnn slv \ {{x}} ⊆ a);
     decide (x ∈ getAnn slv);
     try decide (forall n y, get Y n y -> live_exp_sound y a);
     try dec_solve.
-  + edestruct IHs1; eauto; try inv an; eauto;
-    edestruct IHs2; eauto; try inv an; eauto;
-    decide ((of_list Z) ⊆ getAnn slv1);
-    decide (getAnn slv2 ⊆ a); try dec_solve.
-    decide (isFunctional i).
-    decide ((getAnn slv1 \ of_list Z) ⊆ a).
-    - left; econstructor; eauto. destruct if; eauto.
-    - right; intro. inv H; eauto. destruct if in H10; eauto.
-    - left; econstructor; eauto. destruct if; eauto. intuition.
-    Grab Existential Variables. eassumption. eassumption. eassumption. eassumption.
+  + edestruct (IH s0); eauto; try inv an; eauto; try dec_solve.
+    decide (length s = length sa); try dec_solve;
+    decide (getAnn slv ⊆ a); try dec_solve.
+    exploit (@indexwise_R_dec' _ _
+                               (fun Zs a0 => live_sound i (live_globals s sa ++ Lv) (snd Zs) a0) s sa).
+    intros. eapply IH; eauto. inv an; eauto. destruct X; try dec_solve.
+    exploit (@indexwise_R_dec' _ _
+                               (fun Zs a0 =>  of_list (fst Zs)[<=]getAnn a0 /\
+                                             (if isFunctional i then
+                                                getAnn a0 \ of_list (fst Zs)[<=]a
+                                              else True)) s sa).
+    intros. decide (of_list (fst a0)[<=]getAnn b); try dec_solve.
+    destruct if; try dec_solve. decide ( getAnn b \ of_list (fst a0)[<=]a); try dec_solve.
+    destruct X; try dec_solve.
+    Grab Existential Variables. eassumption. eassumption.
 Defined.
 
 Instance live_sound_dec_inst i Lv s slv `{Computable(annotation s slv)}
@@ -64,14 +70,15 @@ Defined.
 Definition true_live_sound_dec i Lv s slv (an:annotation s slv)
       : Computable (true_live_sound i Lv s slv).
 Proof.
-  general induction s; destruct slv; try isabsurd.
-  + edestruct IHs; eauto; try inv an; eauto;
+  revert i Lv slv an.
+  sind s; intros; destruct s; destruct slv; try isabsurd.
+  + edestruct (IH s); eauto; try inv an; eauto;
     decide (getAnn slv\{{x}} ⊆ a);
     decide (x ∈ getAnn slv -> live_exp_sound e a);
     decide (x ∉ getAnn slv -> a ⊆ getAnn slv\{{x}});
     try dec_solve.
-  + edestruct IHs1; try inv an; eauto;
-    edestruct IHs2; try inv an; eauto;
+  + edestruct (IH s1); try inv an; eauto;
+    edestruct (IH s2); try inv an; eauto;
     decide (exp2bool e = None -> live_exp_sound e a);
     decide (exp2bool e <> Some false -> getAnn slv1 ⊆ a);
     decide (exp2bool e <> Some true -> getAnn slv2 ⊆ a);
@@ -86,19 +93,26 @@ Proof.
       get_functional; subst; eauto.
     - left; econstructor; eauto. destruct if; eauto. intuition.
   + decide(live_exp_sound e a); try dec_solve.
-  + edestruct IHs; eauto; try inv an; eauto;
+  + edestruct (IH s); eauto; try inv an; eauto;
     decide (getAnn slv \ {{x}} ⊆ a);
     try decide (forall n y, get Y n y -> live_exp_sound y a);
     try dec_solve.
-  + edestruct IHs1; eauto; try inv an; eauto;
-    edestruct IHs2; eauto; try inv an; eauto;
-    decide (getAnn slv2 ⊆ a); try dec_solve.
-    decide (isFunctional i).
-    decide ((getAnn slv1 \ of_list Z) ⊆ a).
-    - left; econstructor; eauto. destruct if; eauto.
-    - right; intro. inv H; eauto. destruct if in H10; eauto.
-    - left; econstructor; eauto. destruct if; eauto. intuition.
-    Grab Existential Variables. eassumption. eassumption. eassumption. eassumption.
+  + edestruct (IH s0); eauto; try inv an; eauto; try dec_solve.
+    decide (length s = length sa); try dec_solve;
+    decide (getAnn slv ⊆ a); try dec_solve.
+    exploit (@indexwise_R_dec' _ _
+                               (fun Zs a0 =>
+                                  true_live_sound i (live_globals s sa ++ Lv) (snd Zs) a0) s sa).
+    intros. eapply IH; eauto. inv an; eauto. destruct X; try dec_solve.
+    exploit (@indexwise_R_dec' _ _
+                               (fun Zs a0 => if isFunctional i then
+                                                getAnn a0 \ of_list (fst Zs)[<=]a
+                                              else True) s sa).
+    intros.
+    destruct if; try dec_solve.
+    decide ( getAnn b \ of_list (fst a0)[<=]a); try dec_solve. left; eauto. right; eauto.
+    destruct X; try dec_solve.
+    Grab Existential Variables. eassumption. eassumption.
 Defined.
 
 Instance true_live_sound_dec_inst i Lv s slv `{Computable(annotation s slv)}
