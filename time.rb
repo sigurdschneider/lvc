9823bbaeb35cf211ce81c8293be0ae17cb09b6d7
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2016-04-17 18:26:35 +0200

    Cleanup time.rb (a bit)

--- time.rb
+++ time.rb
@@ -4,6 +4,26 @@ require 'open3'
 
 include Term::ANSIColor
 
+def col(width, text) 
+  return text + "".ljust(width - uncolored(text).length)
+end
+
+def timefile(mod) 
+	return mod.gsub(/(.*)(\/)(.*)/, '\1/.\3') + ".time"
+end
+
+def readETA(mod)
+	est = File.readable?(timefile(mod)) ? File.read(timefile(mod)).strip : ""
+  eta = (Time.now + est.to_i).strftime("%H:%M:%S")
+  return est, eta 
+end
+
+def writeETA(mod, time)
+  File.open(timefile(mod), File::CREAT|File::TRUNC|File::RDWR, 0644) do |file|
+    file.puts "#{time.round(2)}"
+  end
+end	
+
 begin
 parallel = false
 
@@ -23,15 +43,11 @@ end
 
 cmd = ARGV.join(' ')
 mod = ARGV.last
-timefile = mod.gsub(/(.*)(\/)(.*)/, '\1/.\3') + ".time"
-pad = "".ljust(30 - mod.length)
-
-est = File.readable?(timefile) ? File.read(timefile) : ""
-eta = (Time.now + est.to_i).strftime("%H:%M:%S")
-pad2 = "".ljust(12 - est.strip.length)
-eststr = "#{cyan(est.strip)}#{pad2}" + (parallel ? (est.strip == "" ? blue("ETA unavailable") : "ETA #{eta}") : "")
 timestamp = Time.now.strftime("%H:%M:%S") 
-print "#{timestamp} #{cyan('>>>')} #{mod}#{pad}#{eststr}#{(parallel ? "\n" : "")}"
+
+est, eta = readETA(mod) 
+eststr = "#{col(12, cyan(est))}" + (parallel ? (est == "" ? blue("ETA unavailable") : "ETA #{eta}") : "")
+print "#{timestamp} #{cyan('>>>')} #{col(30, mod)}#{eststr}#{(parallel ? "\n" : "")}"
 
 start = Time.now
 cstdin, cstdout, cstderr, waitthr = Open3.popen3("bash -c \"time #{cmd}\"")
@@ -44,7 +60,6 @@ user = serr.match(/.*user[ \t]*([0123456789]+)m([0123456789\.]+)s.*/m)
 sys = serr.match(/.*sys[ \t]*([0123456789]+)m([0123456789\.]+)s.*/m)
 cpu = user[1].to_f * 60 + user[2].to_f + sys[1].to_f * 60 + sys[2].to_f
 timing = "#{cpu.round(2)}"
-pad2 = "".ljust(12 - timing.length)
 changesec = (time - est.to_f)
 changesecstr = sprintf("%+.2f (%+.1f%%)", changesec, (100*changesec/est.to_f)) 
 change = est == "" ? blue("n/a") : (changesec <= 0 ? green(changesecstr) : red(changesecstr))
@@ -52,25 +67,22 @@ line_count = `wc -l "#{mod}.v"`.strip.split(' ')[0].to_i
 spl = (line_count / cpu).round(0)
 speed = success ? ("#{line_count} L,".rjust(9) + "#{spl} L/s".rjust(8)) : ""
 if success then
-  File.open(timefile, File::CREAT|File::TRUNC|File::RDWR, 0644) do |file|
-    file.puts "#{cpu.round(2)}"
-  end
+	writeETA(mod, cpu)
 end
 
 if !parallel then
-	print color.call(timing), pad2, change, ("".ljust(15 - changesecstr.strip.length)), speed, "\n"
+	print col(12, color[timing]), col(15, change), speed, "\n"
 end
 
 sout = cstdout.read
-indent = "  "
 
 if !sout.strip.empty? then
-	print "#{Time.now.strftime("%H:%M:%S")} ", color.call("==="), " #{mod}#{pad} ", "OUTPUT FOLLOWS" , "\n"
+	print "#{Time.now.strftime("%H:%M:%S")} ", color["==="], " #{col(30, mod)} ", "OUTPUT FOLLOWS" , "\n"
   print sout.gsub!(/^/, '  ')
 end
 
 if parallel then
-	print "#{Time.now.strftime("%H:%M:%S")} ", color.call("<<<"), " #{mod}#{pad}", color.call(timing), pad2, change, ("".ljust(15 - changesecstr.strip.length)), speed, "\n"
+	print "#{Time.now.strftime("%H:%M:%S")} ", color["<<<"], " #{col(30, mod)}", col(12, color[timing]), col(15, change), speed, "\n"
 end
 
 exit success
