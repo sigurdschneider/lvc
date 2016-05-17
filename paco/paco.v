b723e58fff2389530a68cbbd0b95915f5d647f6a
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2016-05-17 15:54:35 +0200

    Remove dependency on packN, N > 5

--- paco/paco.v
+++ paco/paco.v
@@ -4,7 +4,7 @@ Require Export paco1.
 Require Export paco2.
 Require Export paco3.
 Require Export paco4.
-Require Export paco5.
+(*Require Export paco5.
 Require Export paco6.
 Require Export paco7.
 Require Export paco8.
@@ -14,4 +14,4 @@ Require Export paco11.
 Require Export paco12.
 Require Export paco13.
 Require Export paco14.
-Require Export paco15.
+Require Export paco15.*)
