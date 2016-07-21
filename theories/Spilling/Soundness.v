61e619a86878e79d703c8d84207f8ec3a6ff685e
Julian Rosemann <s9jnrose@stud.uni-saarland.de>
2016-07-21 11:38:18 +0200

    refactoring in spilling

---
+++ theories/Spilling/Soundness.v
@@ -0,0 +1,31 @@
+Require Import List Map Env AllInRel Exp MoreExp.
+Require Import IL Annotation InRel AutoIndTac Liveness LabelsDefined.
+Require Import SimI.
+
+Fixpoint write_spills (slot: var->var) (xs:list var) (s:stmt) : stmt :=
+match xs with
+| nil => s
+| x::xs => stmtLet (slot x) (Var x) (write_spills slot xs s) 
+end.
+
+Fixpoint write_loads (slot: var-> var) (xs:list var) (s:stmt) : stmt :=
+match xs with
+| nil => s
+| x::xs => stmtLet x (Var (slot x)) (write_loads slot xs s)
+end.
+
+
+Fixpoint doSpill (slot: var -> var) (s:stmt) (a:ann (set var * set var)) : stmt :=
+match s, a with
+| stmtLet x e s, ann1 (Sp,L) a =>
+    write_spills slot (to_list Sp)
+                 (write_loads slot (to_list L) (stmtLet x e (doSpill slot s a)))
+| _,_ => s
+end.
+
+      
+Theorem spill_sound_correct L E E' : spill_sound k Lsl (R,M) s alv asl
+-> (forall x : var, In x R -> E x = E' x)
+-> (forall x : var, In x S -> E x = E' (slot x))
+-> bisim (L,E,s) (L,E', doSpill slot s asl).
+
---
+++ theories/Spilling/Spilling.v
@@ -0,0 +1,80 @@
+Require Import List Map Env AllInRel Exp MoreExp.
+Require Import IL Annotation InRel AutoIndTac Liveness LabelsDefined.
+Require Import SimI.
+
+
+Inductive spill_sound (k:nat) : 
+(list params) -> 
+(list (set var * set var)) ->
+(set var * set var) -> 
+stmt -> 
+ann (set var * set var * option (list (set var * set var)))
+-> Prop :=
+
+| SpillLet ZL Λ R M Sp L K Kx sl x e s
+: spill_sound k ZL Λ ({x;(R\K ∪ L)\Kx }, Sp ∪ M) s sl
+  -> Exp.freeVars e ⊆ R\K ∪ L
+  -> cardinal (R\K ∪ L) <= k
+  -> cardinal ({x;((R\K) ∪ L)\Kx }) <= k
+  -> spill_sound k ZL Λ (R,M) (stmtLet x e s) (ann1 (Sp,L,None) sl)
+
+| SpillReturn ZL Λ R M Sp L K e
+: Exp.freeVars e ⊆ R\K ∪ L
+  -> cardinal ((R\K) ∪ L) <= k
+  -> spill_sound k ZL Λ (R,M) (stmtReturn e) (ann0 (Sp,L,None))
+
+| SpillIf ZL Λ R M Sp L K sl_s sl_t e s t
+: Exp.freeVars e ⊆ R\K ∪ L
+  -> cardinal (R\K ∪ L) <= k
+  -> spill_sound k ZL Λ (R\K ∪ L, Sp ∪ M) s sl_s
+  -> spill_sound k ZL Λ (R\K ∪ L, Sp ∪ M) t sl_t
+  -> spill_sound k ZL Λ (R,M) (stmtIf e s t) (ann2 (Sp,L,None) sl_s sl_t)
+
+| SpillApp ZL Z Λ R M Sp L K f Y R_f M_f
+: cardinal (R\K ∪ L) <= k
+  -> get ZL (counted f) Z
+  -> get Λ (counted f) (R_f,M_f)
+  -> R_f \ of_list Z ⊆ R\K ∪ L
+  -> M_f \ of_list Z ⊆ Sp ∪ M
+    -> spill_sound k ZL Λ (R,M) (stmtApp f Y) (ann0 (Sp,L,None))
+
+| SpillFun (ZL:list params) Λ R M Sp L K t sl_F sl_t (F: list(params*stmt)) rms
+: cardinal (R\K ∪ L) <= k
+  -> (forall n rm, get rms n rm -> cardinal (fst rm) <= k)
+  (*-> R_f ⊆ R\K ∪ Sp ∪ M ∪ of_list Z
+  -> M_f ⊆ R\K ∪ Sp ∪ M ∪ of_list Z*)
+  -> (forall n Zs rm sl_s, get rms n rm 
+     -> get F n Zs
+     -> get sl_F n sl_s
+     -> spill_sound k ((List.map fst F) ++ ZL) (rms ++ Λ) rm (snd Zs) sl_s) 
+  -> spill_sound k ((List.map fst F) ++ ZL) (rms ++ Λ) (R\K ∪ L, Sp ∪ M) t sl_t
+    -> spill_sound k ZL Λ (R,M) (stmtFun F t)
+                   (annF (Sp,L,Some rms) sl_F sl_t)
+.
+
+Inductive fv_e_bounded : nat -> stmt -> Prop :=
+
+| BoundLet k x e s
+: cardinal (Exp.freeVars e) <= k
+  -> fv_e_bounded k s
+  -> fv_e_bounded k (stmtLet x e s)
+
+| BoundReturn k e
+: cardinal (Exp.freeVars e) <= k
+  -> fv_e_bounded k (stmtReturn e)
+
+| BoundIf k e s t
+: cardinal (Exp.freeVars e) <= k
+  -> fv_e_bounded k s
+  -> fv_e_bounded k t
+  -> fv_e_bounded k (stmtIf e s t)
+
+| BoundApp k f Y
+: fv_e_bounded k (stmtApp f Y)
+
+| BoundFun k F t 
+: (forall n Zs, get F n Zs -> fv_e_bounded k (snd Zs))
+  -> fv_e_bounded k t
+  -> fv_e_bounded k (stmtFun F t)
+.
+
