59496fb8fd8ad9eee06a609f0e0c72fda2a79845
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2016-08-01 14:00:27 +0200

    Improve count admits.

--- countAdmits.sh
+++ countAdmits.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-NUM=`grep "admit" $(find . -type f | grep -v '\.glob' | grep -v '\.sh') | wc -l`
-
-echo "There are $NUM admits left in the developement" 
+grep -c "admit" $(find . -type f -iname '*.v') | sed '/:0$/d'
