0cf1b48c3a19325dc9ea469fd6b8071928ca43ab
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2016-12-12 18:24:02 +0100

    Refactor slot record.

---
+++ theories/Spilling/Slot.v
@@ -0,0 +1,12 @@
+Require Import Nat CSet Var MapInjectivity.
+
+Definition slot n x := x + n.
+
+Inductive Slot (VD:set var) :=
+  {
+    Slot_slot :> var -> var;
+    Slot_Disj : disj VD (SetConstructs.map Slot_slot VD);
+    Slot_Inj : injective_on VD Slot_slot
+  }.
+
+Hint Immediate Slot_Disj Slot_Inj.
