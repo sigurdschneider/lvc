e7cb16d637d9343dfe1bc55744f081aa631979c4
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2016-12-21 17:32:08 +0100

    Remove more paco defs to speed up compilation.

--- paco/paco.v
+++ paco/paco.v
@@ -4,8 +4,3 @@ Require Export paco1.
 Require Export paco2.
 Require Export paco3.
 Require Export paco4.
-Require Export paco5.
-Require Export paco6.
-Require Export paco7.
-Require Export paco8.
-Require Export paco9.
