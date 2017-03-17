94283ebd0d2833856a67e3bbffb8ef097e664678
Julian Rosemann <s9jnrose@stud.uni-saarland.de>
2017-03-17 20:14:55 +0100

    RepairSpill: Progress in invariance proof

--- theories/Spilling/RepairSpill.v
+++ theories/Spilling/RepairSpill.v
@@ -38,7 +38,7 @@ Fixpoint repair_spill
 
   | stmtLet x e s, ann1 _ rlv', ann1 _ lv', ann1 (Sp,L,_) sl'
     => let Fv_e := Exp.freeVars e in
-      let L'   := (M ∩ L) ∪ (Fv_e \ R) in
+      let L'   := (Fv_e \ R) ∪ (L ∩ ((Sp ∩ R) ∪ M)) in
       (* we only use register liveness at 2 points to make some preferences in the selection of the kill set: (here)
          if the register liveness is incorrect we will still get a correct spilling *)
       let K    := of_list (take (cardinal (R ∪ L') - k) (elements (R \ Fv_e \ getAnn rlv')
@@ -46,27 +46,27 @@ Fixpoint repair_spill
       let R_e  := R \ K ∪ L' in
       let K_x  := one_or_empty_if' k R_e (R_e \ getAnn rlv') in
       (* here we need normal liveness, because we have to spilled variables that are loaded later on *)
-      let Sp'  := (R ∩ Sp) ∪ (getAnn lv' ∩ (K ∪ K_x) \ M) in
+      let Sp'  := (getAnn lv' ∩ (K ∪ K_x) \ M) ∪ (Sp ∩ R) in
       let R_s  := {x; R_e \ K_x} in
       ann1 (Sp',L',nil) (repair_spill k ZL Λ R_s (Sp' ∪ M) s rlv' lv' sl')
            
   | stmtReturn e, _, _, ann0 (Sp,L,_)
     => let Fv_e := Op.freeVars e in
-      let L'   := (M ∩ L) ∪ Fv_e \ R in
+      let L'   := Fv_e \ R ∪ (L ∩ ((Sp ∩ R) ∪ M))in
       let K    := set_take (cardinal (R ∪ L') - k) (R \ Fv_e) in
       let R_e  := R \ K ∪ L' in
-      let Sp'  := R ∩ Sp in
+      let Sp'  := Sp ∩ R in
       ann0 (Sp',L',nil)
 
   | stmtIf e s1 s2, ann2 _ rlv1 rlv2, ann2 _ lv1 lv2, ann2 (Sp,L,_) sl1 sl2
     => let Fv_e := Op.freeVars e in
-      let L'   := (M ∩ L) ∪ Fv_e \ R in
+      let L'   := (Fv_e \ R) ∪ (L ∩ ((Sp ∩ R) ∪ M)) in
       (* here is the second use of register liveness *)
       let K    := of_list (take (cardinal (R ∪ L') - k)
                                 (elements (R \ Fv_e \ getAnn rlv1 \ getAnn rlv2)
                                           ++ elements ((getAnn rlv1 ∪ getAnn rlv2) ∩ R \ Fv_e))) in
        let R_e  := R \ K ∪ L' in
-       let Sp'  := (R ∩ Sp) ∪ ((getAnn lv1 ∪ getAnn lv2) ∩ K \ M) in
+       let Sp'  := ((getAnn lv1 ∪ getAnn lv2) ∩ K \ M) ∪ (Sp ∩ R) in
        ann2 (Sp',L',nil)
             (repair_spill k ZL Λ R_e (Sp' ∪ M) s1 rlv1 lv1 sl1)
             (repair_spill k ZL Λ R_e (Sp' ∪ M) s2 rlv2 lv2 sl2)
@@ -75,20 +75,20 @@ Fixpoint repair_spill
     => let R_f := fst (nth (counted f) Λ (∅,∅)) in
       let M_f := snd (nth (counted f) Λ (∅,∅)) in
       let Z   := nth (counted f) ZL nil in
-      let L'  := L ∪ R_f \ R \ of_list Z in
+      let L'  := (R_f \ of_list Z \ R) ∪ (L ∩ ((Sp ∩ R) ∪ M)) in
       let K   := set_take (cardinal (R ∪ L') - k) (R \ R_f) in
-      let Sp' := Sp ∪ M_f \ M \ of_list Z in
-      ann0 (Sp',L',(R',((Sp' ∪ M) ∩ M') ∪ list_union (Op.freeVars ⊝ Y) \ (R \ K ∪ L'))::nil)
+      let Sp' := (M_f  \ of_list Z \ M) ∪ (Sp ∩ R) in
+      ann0 (Sp',L',(R',(Sp' ∪ M) ∩ list_union (Op.freeVars ⊝ Y) \ (R \ K ∪ L') ∪ M')::nil)
 
   | stmtFun F t, annF rLV rlv_F rlv_t, annF LV lv_F lv_t, annF (Sp,L,rms) sl_F sl_t
     => let rms' := stretch_rms k F rms (getAnn ⊝ lv_F) in
       let ZL'  := fst ⊝ F ++ ZL in
       let Λ'   := rms' ++ Λ in
-      let L'   := M ∩ L in
+      let L'   := L ∩ ((Sp ∩ R) ∪ M) in
       (* here is the third use of register liveness *)
       let K    := of_list (take (cardinal (R ∪ L') - k)
                                 (elements (R \ getAnn rlv_t) ++ elements (getAnn rlv_t ∩ R))) in
-      let Sp'  := (R ∩ Sp) ∪ ((getAnn lv_t) ∩ K \ M) in
+      let Sp'  := ((getAnn lv_t) ∩ K \ M) ∪ (Sp ∩ R) in
 
        annF (Sp, L, rms)
             ((fun f rmlvsl
--- theories/Spilling/RepairSpillInv.v
+++ theories/Spilling/RepairSpillInv.v
@@ -1,14 +1,42 @@
-Require Import RepairSpill.
+Require Import RepairSpill RegLive.
 Require Import SpillSound Annotation Liveness.Liveness.
-Require Import List Map IL.
+Require Import List Map IL Take TakeSet OneOrEmpty.
 
 Set Implicit Arguments.
 
 (** * Invariance on correct spillings *)
 
 (* assumptions missing *)
-Lemma repair_spill_inv
+Lemma repair_spill_inv k ZL Λ s lv sl R M G
   : live_sound Imperative ZL (merge ⊝ Λ) s lv
     -> spill_sound k ZL Λ (R,M) s sl
-    -> sl === repair_spill k ZL Λ R M s lv sl
+    -> sl === repair_spill k ZL Λ R M s lv (reg_live (fst ⊝ Λ) ZL G s sl) sl
 .
+Proof.
+  intros liveSnd spillSnd.
+  general induction sl; invc liveSnd; invc spillSnd; econstructor.
+  - econstructor; econstructor; symmetry;
+      apply inter_subset_equal in H9; apply inter_subset_equal in H10.
+    + rewrite H9; erewrite !get_nth; eauto. unfold snd. apply union_subset_equal.
+      rewrite H17. clear; cset_tac.
+    + erewrite !get_nth; eauto. rewrite H9, H10. unfold fst. apply union_subset_equal.
+      rewrite H16. clear; cset_tac.
+    + erewrite !get_nth; eauto. unfold snd, fst. econstructor; eauto.
+      apply union_subset_equal. rewrite H17. unfold set_take; rewrite take_set_incl. admit. (*
+      rewrite meet_comm. apply meet_incl. rewrite H18. cset_tac. rewrite H16.
+      apply incl_eq.
+      * admit.
+      * rewrite H17. apply union_incl_split.
+        -- clear; cset_tac.
+        -- rewrite H9 at 2. rewrite H10. unfold set_take. rewrite take_set_incl.
+           rewrite H18. rewrite H16.*)
+
+    + econstructor; eauto.      
+  - econstructor; econstructor; symmetry; apply inter_subset_equal in H6;
+      eauto.
+    apply inter_subset_equal in H7. rewrite H6, H7. apply union_subset_equal.
+    clear - H8. cset_tac.
+  - econstructor; econstructor; symmetry;
+      apply inter_subset_equal in H12; apply inter_subset_equal in H13.
+    + rewrite H12 at 6. apply union_subset_equal. admit. 
+    + rewrite H12, H13. apply union_subset_equal. clear - H15; cset_tac.
