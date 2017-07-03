467ba81c387e3e02f166c6b9148fb0be09cc55d5
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2017-07-03 15:18:25 +0200

    Improve inlining for extraction

--- compiler/extraction.v
+++ compiler/extraction.v
@@ -1,13 +1,21 @@
 Require Import ExtrOcamlBasic ExtrOcamlString.
 (*Require Import ExtrOcamlZInt.
 Require Import ExtrOcamlNatInt.*)
-Require Import Compiler OrderedType Parmov.
-
+Require Import Compiler OrderedType Parmov Infra.Lattice Status CSetPartialOrder.
+Require AnalysisBackward AnnotationLattice LivenessAnalysis.
 (* Unset Extraction AccessOpaque. *)
 
 Definition foo := True.
 
-Extraction Inline Status.bind Option.bind.
+Extraction Inline Status.bind Option.bind Computable.inst_not_cm bottom
+           CSetPartialOrder.PartialOrder_Subset_Equal AnalysisBackward.makeBackwardAnalysis.
+
+(*Extraction Inline PartialOrder_sig poLe poEq poLe_dec poEq_dec PartialOrder_Subset_Equal_Bounded
+           Annotation.PartialOrder_ann SOT_as_OT SOT_cmp.*)
+
+Extraction Inline SOT_as_OT SOT_cmp.
+
+Extraction Inline LivenessAnalysis.liveness_transform_dep set_var_lower_bounded.
 
 Extraction Blacklist List String Int.
 
