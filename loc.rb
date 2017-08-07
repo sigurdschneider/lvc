755dd2d47fd9ce2183ea767bc08a8d24bb005e54
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2017-08-07 17:56:37 +0200

    Fix loc script

--- loc.rb
+++ loc.rb
@@ -39,33 +39,38 @@ end
 
 @texcmds = File.open("loc.tex", 'w')
 
-def comp(name, paths, exts=Set.new([".v"]))
+def comp(name, paths, exts=Set.new([".v"]), silent=false)
   l, c = loc(paths, exts)
 	@total["loc"] += l
 	@total["count"] += c
-  print str(l,c), " ", name, "\n"
+	if not silent then print str(l,c), " ", name, "\n" end
   @texcmds.write("\\newcommand{\\#{name.gsub(/\s+/,"")}}{#{l}}\n");
 end
 
-comp("External Dependecies", ["paco/*", "ContainersPlugin/"], [".v", ".ml4"])
+comp("paco", ["paco/*"], [".v", ".ml4"], true)
+comp("containers", ["ContainersPlugin/"], [".v", ".ml4"], true)
+ext=@total
 @total = Hash.new(0)
-comp("Libraries and Tactics", ["theories/Constr", "theories/Infra"])
+comp("Sets and Maps", ["theories/Constr"])
+comp("Utilities and Tactics", ["theories/Infra"])
 comp("Semantics", ["theories/IL", "theories/Isa"])
 comp("Equivalence", ["theories/Equiv"])
+comp("Coherence", ["theories/Coherence/Coherence.v", "theories/Coherence/Restrict.v", "theories/Coherence/Coherence_*.v"])
 comp("Analyses", ["theories/Analysis", "theories/Liveness", "theories/Reachability"])
 comp("Value Optimizations", ["theories/ValueOpts"])
 comp("Dead Code Elimination", ["theories/DVE.v", "theories/UCE.v", "theories/DCVE.v", "theories/DCE.v"])
-comp("Register Assignment", ["theories/Coherence/Allocation*"])
+comp("Register Assignment", ["theories/Coherence/Allocation*", "theories/RegAssign.v", "theories/RenameApartToPart.v"])
 comp("Spilling", ["theories/Spilling"])
 comp("Lowering", ["theories/Lowering"])
 comp("Alpha Equivalence", ["theories/Alpha"])
 comp("SMT Translation Validation", ["theories/TransVal"])
-comp("SSA Construction", ["theories/Coherence/Coherence.v", "theories/Coherence/Delocation*", "theories/Coherence/Invariance.v",
-                          "theories/Coherence/Restrict.v"])
-comp("OCaml Integration", ["compiler/*.ml", "compiler/*.mll", "compiler/*.v", "compiler/*.mly"], [".ml", ".v", ".mll", ",mly"])
+comp("SSA Construction", ["theories/Coherence/AddParam.v", "theories/Coherence/Delocation*", "theories/Coherence/Invariance.v",
+                          "theories/Coherence/AddParams.v", "theories/Coherence/AddAdd.v"])
+comp("OCaml Integration", ["compiler/*.ml", "compiler/*.mll", "compiler/*.v", "compiler/*.mly", "theories/Compiler.v"], [".ml", ".v", ".mll", ",mly"])
 comp("Coq Plugin", ["src/*.ml4"], [".ml4"])
 
-print @total["loc"],  " loc in ", @total["count"], " files in total\n"
+print @total["loc"],  " loc in ", @total["count"], " files in LVC\n"
+print ext["loc"],  " loc in ", ext["count"], " files in external dependencies\n"
 
 Find.find("theories/") do |path|
   if File.extname(path) == ".v" then
