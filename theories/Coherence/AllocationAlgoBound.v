4fffeb3016dddfe6e058bbc7e44acbd7157d047a
Sigurd Schneider <sigurd.schneider@cs.uni-saarland.de>
2017-09-26 16:20:41 +0200

    More progress

--- theories/Coherence/AllocationAlgoBound.v
+++ theories/Coherence/AllocationAlgoBound.v
@@ -285,6 +285,53 @@ Require Import AllocationAlgoCorrect AnnP BoundedIn VarP.
 
 Definition regbnd p k (x:positive) := part_1 p x -> (x <= Pos.of_nat (2*k))%positive.
 
+Instance ann_P_morph A (P:A->Prop) (R:A->A->Prop) (H:forall x y, R x y -> P x -> P y)
+  : Proper (ann_R R ==> impl) (ann_P (A:=A) P).
+Proof.
+  unfold Proper, respectful, impl.
+  intros.
+  general induction H0; invt ann_P; eauto using ann_P.
+  econstructor; intros; inv_get; eauto using ann_P.
+Qed.
+
+
+Lemma mapAnn_renamedApart al ans s (ϱ ϱ': var -> var)
+      (P1:Proper (_eq ==> _eq) ϱ) (P2:Proper (_eq ==> _eq) ϱ')
+      (AR:ann_R Subset1 al ans)
+      (RA:renamedApart s ans)
+      (AGR:agree_on _eq (fst (getAnn ans) ∪ snd (getAnn ans)) ϱ ϱ')
+:  ann_R SetInterface.Equal (mapAnn (lookup_set ϱ) al)
+         (mapAnn (lookup_set ϱ') al).
+Proof.
+  general induction RA; invt @ann_R; pe_rewrite; simpl in *; set_simpl.
+  - econstructor; eauto.
+    + eapply lookup_set_agree; eauto.
+      simpl. eapply agree_on_incl; eauto. eauto with cset.
+    + eapply IHRA; eauto using agree_on_incl with cset.
+  - econstructor; eauto.
+    + eapply lookup_set_agree; eauto.
+      simpl. eapply agree_on_incl; eauto. eauto with cset.
+    + eauto using agree_on_incl with cset.
+    + eauto using agree_on_incl with cset.
+  - econstructor; eauto.
+    + eapply lookup_set_agree; eauto.
+      simpl. eapply agree_on_incl; eauto.
+  - econstructor; eauto.
+    + eapply lookup_set_agree; eauto.
+      simpl. eapply agree_on_incl; eauto.
+  - econstructor; eauto.
+    + eapply lookup_set_agree; eauto.
+      simpl. eapply agree_on_incl; eauto. rewrite <- H9. clear; cset_tac.
+    + eauto with len.
+    + intros; inv_get.
+      eapply H1; eauto.
+      simpl. eapply agree_on_incl; eauto.
+      eapply ans_incl_D_union; eauto.
+    + eapply IHRA; eauto.
+      simpl. eapply agree_on_incl; eauto.
+      clear; cset_tac.
+Qed.
+
 
 Lemma regAssign_assignment_small k p VD (ϱ:Map [var,var]) ZL Lv s alv ϱ' ra
       (LS:live_sound FunctionalAndImperative ZL Lv s alv)
@@ -301,7 +348,11 @@ Proof.
   - econstructor; eauto.
     + exploit regAssign_renamedApart_agree; eauto using live_sound.
       pe_rewrite.
-      admit.
+      rewrite <- map_update_update_agree in H2.
+      eapply agree_on_update_inv in H2.
+      rewrite <- lookup_set_agree; swap 1 4.
+      change _eq with (@eq var). eapply agree_on_incl; eauto. revert H10 H7; clear; cset_tac.
+      eauto. eauto. eauto.
     + eapply IHLS; eauto.
       * eapply injective_on_agree; [|eapply map_update_update_agree].
         eapply injective_on_update_fresh; eauto using injective_on_incl.
@@ -309,7 +360,23 @@ Proof.
       * rewrite <- map_update_update_agree.
         eapply sep_update_part.
         eauto using sep_incl.
-      * admit.
+      * hnf; intros.
+        rewrite <- lookup_set_agree in H2; swap 1 4.
+        eapply map_update_update_agree. eauto. eauto.
+        assert (EQal:getAnn al [=] getAnn al \ singleton x ∪ singleton x). {
+          revert H1. clear.
+          cset_tac.
+        }
+        eapply lookup_set_morphism_eq in H2; [|rewrite EQal; reflexivity].
+        rewrite lookup_set_union in H2. eapply union_iff in H2; destruct H2.
+        -- rewrite lookup_set_agree in H2; swap 1 4.
+           eapply agree_on_update_dead. clear. cset_tac. reflexivity. eauto. eauto.
+           eapply up. rewrite <- H0. eauto.
+        -- rewrite lookup_set_singleton' in H2; eauto. eapply In_single in H2.
+           invc H2. lud; [|isabsurd].
+           hnf; intros. eapply ann_P_get in H5.
+           admit.
+        -- eauto.
   - monadS_inv allocOK.
     exploit regAssign_renamedApart_agree; eauto using live_sound. pe_rewrite.
     exploit regAssign_renamedApart_agree; try eapply EQ; eauto using live_sound. pe_rewrite.
@@ -321,7 +388,13 @@ Proof.
     + exploit IHLS1; eauto.
       * eauto using injective_on_incl; eauto.
       * rewrite H0. eauto.
-      * admit.
+      * eapply ann_P_morph with (R:=SetInterface.Equal); eauto.
+        intros. rewrite <- H17. eauto.
+        eapply mapAnn_renamedApart; eauto.
+        eapply agree_on_incl; eauto.
+        pe_rewrite. rewrite <- disj_eq_minus; try reflexivity.
+        eapply disj_union_left. symmetry. eapply renamedApart_disj in H12. pe_rewrite. eauto.
+        symmetry; eauto.
     + exploit IHLS2; try eapply EQ0; eauto.
       * eapply injective_on_agree; swap 1 2.
         change _eq with (@eq var).
@@ -341,10 +414,83 @@ Proof.
     intros. eapply regAssign_renamedApart_agree'; eauto using live_sound.
     reflexivity.
     econstructor; eauto.
-    + admit.
+    + rewrite <- lookup_set_agree; eauto.
+      simpl.
+      etransitivity; eauto using agree_on_incl.
+      eapply agree_on_incl; eauto.
+      rewrite <- disj_eq_minus; try reflexivity.
+      rewrite H19.  eapply disj_D_defVars; eauto.
     + intros. inv_get.
-      admit.
-    + admit.
+      edestruct regAssignF_get; eauto.
+      * intros. exploit disj_D_defVars; eauto.
+        eapply renamedApart_disj in H13. pe_rewrite.
+        eapply defVars_disj_D; eauto. eapply disj_union_right; eauto.
+      * dcr. instantiate (1:=fst (getAnn x1) ∪ snd (getAnn x1)) in H27.
+        rewrite <- disj_eq_minus in H27; try reflexivity; swap 1 2.
+        {
+          edestruct H11; dcr; eauto.
+          rewrite H20. setoid_rewrite union_comm at 2. rewrite union_assoc.
+          eapply disj_union_left; symmetry.
+          eapply disj_D_defVars_take; eauto.
+          exploit defVars_take_disj; eauto.
+        }
+        rewrite <- map_update_list_update_agree' in H27; eauto with len.
+        assert (INCLx0:getAnn x0 ⊆ fst (getAnn x1) ∪ snd (getAnn x1)). {
+          exploit H22; eauto.
+          eapply ann_R_get in H20. rewrite H20. eauto with cset.
+        }
+        assert (DECOMP:getAnn x0 [=] getAnn x0 \ of_list (fst x2) ∪ of_list (fst x2)). {
+          edestruct H2; eauto; dcr.
+          revert H20; clear; cset_tac.
+        }
+        assert (Inclx2:getAnn x0 \ of_list (fst x2) ⊆ lv). {
+          edestruct H2; eauto; dcr.
+        }
+        exploit H1; eauto.
+        -- eapply injective_on_agree; simpl; eauto using agree_on_incl.
+           rewrite DECOMP at 1.
+           eapply injective_on_fresh_list. eauto.
+           eapply injective_on_incl; eauto. eauto with len.
+           eapply disj_intersection.
+           eapply fresh_list_stable_spec. simpl.
+           edestruct H2; eauto.
+           eapply fresh_list_stable_nodup.
+        -- eapply sep_agree; eauto.
+           eapply agree_on_incl; eauto.
+           rewrite DECOMP at 1.
+           eapply sep_update_list; eauto.
+           edestruct H2; eauto.
+           eapply sep_incl; eauto.
+           rewrite <- Inclx2. clear. cset_tac.
+        -- rewrite <- lookup_set_agree; swap 1 4; eauto using agree_on_incl.
+           hnf; intros.
+           rewrite lookup_set_update_with_list_in_union_length in H20; eauto with len.
+           eapply union_iff in H20; destruct H20.
+           eapply up; eauto. rewrite <- Inclx2. eauto.
+           hnf; intros. admit.
+        -- eapply ann_P_morph with (R:=SetInterface.Equal); eauto.
+           intros. rewrite <- H26. eauto.
+           eapply mapAnn_renamedApart; eauto.
+           eapply regAssign_renamedApart_agreeF' with (ans:=drop (S n ) ans) in H24;
+             eauto; try reflexivity; intros; inv_get; eauto with len.
+           ++
+    + exploit IHLS; try eapply EQ0; eauto.
+      * eapply injective_on_agree; swap 1 2.
+        change _eq with (@eq var).
+        eapply agree_on_incl. eapply H5. etransitivity; eauto.
+        rewrite <- disj_eq_minus; try reflexivity.
+        rewrite H19.  eapply disj_D_defVars; eauto.
+        eauto using injective_on_incl; eauto.
+      * eapply sep_incl; eauto.
+        etransitivity; eauto.
+        rewrite <- disj_eq_minus; try reflexivity.
+        rewrite H19.  eapply disj_D_defVars; eauto.
+      * rewrite <- lookup_set_agree; swap 1 4.
+        eapply agree_on_incl; eauto.
+        etransitivity; eauto.
+        rewrite <- disj_eq_minus; try reflexivity.
+        rewrite H19.  eapply disj_D_defVars; eauto. eauto. eauto.
+        rewrite H3. eauto.
 Qed.
 
 Lemma regAssign_assignment_small k p VD (ϱ:Map [var,var]) ZL Lv s alv ϱ'
